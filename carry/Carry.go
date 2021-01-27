e7c2f571b282440896f5af5a9894d7a3ec6c18ba
张浩伟 <haoweizh@gmail.com>
2021-01-27 18:40:33 +0800

    add carry place order process

--- carry/Carry.go
+++ carry/Carry.go
@@ -11,14 +11,14 @@ import (
 	"time"
 )
 
-// open amount: min(usd value - all current other coin value, best bid amount, best ask amount)
-// open price: lose 0.0014 price distance < last 6 hour funding rate * 4, ioc
-// close price: win 0.0014 price distance or last 6 hour funding rate * 4, market
-const FTXFee = 0.0007
+const FTXHighOpen = 0.004
+const FTXLowOpen = -0.006
+const FTXClose = 0.001
+const OrderLimitUsd = 10.0
 
 var carryLock sync.Mutex
 var carrying bool
-var wealth, usdValue float64
+var holding, usdAvailable float64
 
 var perpSnapshot = make(map[string]float64)
 
@@ -39,13 +39,13 @@ var ProcessCarry = func(setting *model.Setting) {
 	_, tickPerp := model.AppMarkets.GetBidAsk(setting.Symbol, setting.Market)
 	symbolRelated := setting.GetRelatedSymbol()
 	_, tickRelated := model.AppMarkets.GetBidAsk(symbolRelated, setting.Market)
-	markedInfo := model.MarketInfos[setting.Market][setting.Symbol]
+	marketInfo := model.MarketInfos[setting.Market][setting.Symbol]
 	marketInfoRelated := model.MarketInfos[setting.Market][symbolRelated]
 	now := util.GetNowUnixMillion()
 	if tickPerp == nil || tickRelated == nil || tickPerp.Asks == nil || tickPerp.Bids == nil ||
 		tickRelated.Asks == nil || tickRelated.Bids == nil || model.AppConfig.Handle != `1` ||
 		model.AppPause || now-int64(tickRelated.Ts) > 1000 || now-int64(tickPerp.Ts) > 1000 ||
-		markedInfo == nil || marketInfoRelated == nil {
+		marketInfo == nil || marketInfoRelated == nil {
 		return
 	}
 	if setting == nil || isCarrying() {
@@ -54,58 +54,106 @@ var ProcessCarry = func(setting *model.Setting) {
 	setCarrying(true)
 	defer setCarrying(false)
 	rates, _ := api.GetFundingRate(setting.Market, setting.Symbol)
-	marketSymbols := model.GetMarketSymbols(setting.Market)
 	rateSum := 0.0
 	for _, item := range rates.([]*model.FundingRate) {
 		rateSum += item.Rate
 	}
-	if wealth == 0 || usdValue == 0 {
+	if holding == 0 || usdAvailable == 0 {
 		balances := api.GetBalance(``, ``, setting.Market, 0)
 		for _, value := range balances {
-			wealth += value.UsdValue
 			if strings.ToLower(value.Coin) == `usd` {
-				usdValue = value.Amount
+				usdAvailable = value.Amount
+			} else {
+				holding += math.Abs(value.UsdValue)
 			}
 		}
-		util.Notice(fmt.Sprintf(`[carry] set wealth %f usd %f`, wealth, usdValue))
+		usdAvailable = (usdAvailable - holding) / 2
+		util.Notice(fmt.Sprintf(`[carry] set holding %f usd %f`, holding, usdAvailable))
 		return
 	}
-	openAmount := math.Min((usdValue-wealth/2)/tickPerp.Asks[0].Price,
-		math.Min(tickPerp.Bids[0].Amount/2, tickRelated.Asks[0].Amount/2))
-	score := 1 - tickRelated.Asks[0].Price*(1+2*FTXFee)/tickPerp.Bids[0].Price + rateSum
+	score := math.Max(1-tickRelated.Asks[0].Price/tickPerp.Bids[0].Price+rateSum, 0)
+	if rateSum < 0 {
+		score = math.Min(1-tickRelated.Bids[0].Price/tickPerp.Asks[0].Price+rateSum, 0)
+	}
 	perpSnapshot[setting.Symbol] = score
-	highestScore := 0.0
-	highestSymbol := ``
+	var scoreHigh, scoreLow float64
+	var symbolHigh, symbolLow string
 	for symbol, value := range perpSnapshot {
-		if value > highestScore {
-			highestSymbol = symbol
+		if value > scoreHigh {
+			symbolHigh = symbol
+			scoreHigh = value
+		} else if value < scoreLow {
+			symbolLow = symbol
+			scoreLow = value
 		}
 	}
-	if highestSymbol == setting.Symbol {
-		model.SetCarryInfo(`grid`, fmt.Sprintf(`not valid %s len symbols:%f scores:%d score %f `,
-			highestSymbol, 0.9*float64(len(marketSymbols)), len(perpSnapshot), score))
-		if openAmount < markedInfo.SizeIncrement || openAmount < marketInfoRelated.SizeIncrement {
-			util.Notice(fmt.Sprintf(`size not enough order size %f < %s size %f or %s size %f score %f`,
-				openAmount, markedInfo.Name, markedInfo.SizeIncrement, marketInfoRelated.Name,
-				marketInfoRelated.SizeIncrement, score))
-			time.Sleep(time.Minute)
-			return
+	sidePerp, sideRelated, amount := calcCarryOpen(setting, marketInfo, marketInfoRelated, tickPerp,
+		tickRelated, symbolHigh, symbolLow, score, scoreHigh, scoreLow)
+	if amount > 0 {
+		util.Notice(fmt.Sprintf(`carry between %s %s with score %f rate sum %f amount %f worth %f`,
+			setting.Symbol, symbolRelated, score, rateSum, amount, amount*tickPerp.Asks[0].Price))
+		perpPrice := tickPerp.Asks[0].Price
+		relatedPrice := tickRelated.Bids[0].Price
+		if sidePerp == model.OrderSideSell {
+			setting.GridAmount += amount
+			perpPrice = tickPerp.Bids[0].Price
+			relatedPrice = tickRelated.Asks[0].Price
+		} else if sidePerp == model.OrderSideBuy {
+			setting.GridAmount -= amount
+			perpPrice = tickPerp.Asks[0].Price
+			relatedPrice = tickRelated.Bids[0].Price
 		}
-	}
-	if highestSymbol == setting.Symbol && score > 0.002 &&
-		float64(len(perpSnapshot)) > 0.45*float64(len(marketSymbols)) {
-		util.Notice(fmt.Sprintf(`carry from %s to %s with score %f rate sum %f amount %f worth %f`,
-			setting.Symbol, symbolRelated, score, rateSum, openAmount, openAmount*tickRelated.Bids[0].Price))
-		go api.PlaceOrder(``, ``, model.OrderSideSell, model.OrderTypeMarket, setting.Market, setting.Symbol,
-			``, ``, ``, ``, model.FunctionCarry, tickPerp.Bids[0].Price,
-			tickPerp.Bids[0].Price, openAmount, true)
-		go api.PlaceOrder(``, ``, model.OrderSideBuy, model.OrderTypeMarket, setting.Market, symbolRelated,
-			``, ``, ``, ``, model.FunctionCarry, tickRelated.Asks[0].Price,
-			tickRelated.Asks[0].Price, openAmount, true)
-		setting.GridAmount += openAmount
+		go api.PlaceOrder(``, ``, sidePerp, model.OrderTypeMarket, setting.Market, setting.Symbol, ``,
+			``, ``, ``, model.FunctionCarry, perpPrice, perpPrice, amount, true)
+		go api.PlaceOrder(``, ``, sideRelated, model.OrderTypeMarket, setting.Market, symbolRelated, ``,
+			``, ``, ``, model.FunctionCarry, relatedPrice, relatedPrice, amount, true)
 		model.AppDB.Save(&setting)
-		wealth = 0
-		usdValue = 0
+		holding = 0
+		usdAvailable = 0
 		time.Sleep(time.Second * 30)
 	}
+}
+
+func calcCarryOpen(setting *model.Setting, marketInfo, marketInfoRelated *model.MarketInfo, tickPerp, tickRelated *model.BidAsk,
+	symbolHigh, symbolLow string, score, scoreHigh, scoreLow float64) (sidePerp, sideRelated string, amount float64) {
+	//marketSymbols := model.GetMarketSymbols(setting.Market)
+	//if float64(len(perpSnapshot)) < 0.45*float64(len(marketSymbols)) {
+	//	return ``, ``, 0
+	//}
+	var bidAmount, askAmount float64
+	if (scoreLow < FTXLowOpen && setting.Symbol == symbolLow) || (setting.GridAmount > 0 && score < -1*FTXClose) {
+		bidAmount = tickPerp.Asks[0].Amount
+		askAmount = tickRelated.Bids[0].Amount
+		sidePerp = model.OrderSideBuy
+		sideRelated = model.OrderSideSell
+	} else if (scoreHigh > FTXHighOpen && setting.Symbol == symbolHigh) || (setting.GridAmount < 0 && score > FTXClose) {
+		bidAmount = tickRelated.Asks[0].Amount
+		askAmount = tickPerp.Bids[0].Amount
+		sidePerp = model.OrderSideSell
+		sideRelated = model.OrderSideBuy
+	}
+	model.SetCarryInfo(`[grid]`, fmt.Sprintf(`symbo low: %s %f high: %s %f symbols: %d available usd: %f`,
+		symbolLow, scoreLow, symbolHigh, scoreHigh, len(perpSnapshot), usdAvailable))
+	if bidAmount == 0 || askAmount == 0 {
+		return ``, ``, 0
+	}
+	if tickPerp.Asks[0].Price*bidAmount > OrderLimitUsd {
+		bidAmount /= 2
+	}
+	if tickPerp.Asks[0].Price*askAmount > OrderLimitUsd {
+		askAmount /= 2
+	}
+	if setting.Symbol == symbolLow || setting.Symbol == symbolHigh {
+		amount = math.Min(usdAvailable/tickPerp.Asks[0].Price, math.Min(bidAmount, askAmount))
+	} else {
+		amount = math.Min(math.Abs(setting.GridAmount), math.Min(bidAmount, askAmount))
+	}
+	amount = math.Floor(amount/marketInfo.SizeIncrement) * marketInfo.SizeIncrement
+	if amount < marketInfo.SizeIncrement || amount < marketInfoRelated.SizeIncrement {
+		util.Notice(fmt.Sprintf(`size not enough order size %f < %s size %f or %s size %f %s-%f %s-%f`,
+			amount, marketInfo.Name, marketInfo.SizeIncrement, marketInfoRelated.Name,
+			marketInfoRelated.SizeIncrement, symbolLow, scoreLow, symbolHigh, scoreHigh))
+		return ``, ``, 0
+	}
+	return sidePerp, sideRelated, amount
 }
