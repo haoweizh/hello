198fa2e5f77733abf897e873a0faa7aa52be4db8
张浩伟 <haoweizh@gmail.com>
2021-02-11 11:40:40 +0800

    carry ftx, new equal method

--- carry/Carry.go
+++ carry/Carry.go
@@ -79,6 +79,9 @@ func clearCarryBalance() {
 					holding += math.Abs(value.UsdValue)
 				}
 			}
+			if model.AppConfig.Env == `simon` {
+				usdAvailable = (usdAvailable - holding) / 2
+			}
 			util.Notice(fmt.Sprintf(`[carry] usd:%f valuedUsd:%f holding:%f`, usdAvailable, valueUsd, holding))
 		}
 		util.Notice(`...... exit clearing carry balance`)
