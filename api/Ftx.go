7085d6713983c3c4da5d668bbcce824a15c53a47
张浩伟 <haoweizh@gmail.com>
2021-02-24 21:09:22 +0800

    add ftx carry speed monitoring

--- api/Ftx.go
+++ api/Ftx.go
@@ -670,8 +670,11 @@ func placeOrderFtx(order *model.Order, key, secret, orderSide, orderType, accoun
 	postData[`side`] = orderSide
 	postData[`size`], _ = strconv.ParseFloat(amount, 64)
 	postData[`type`] = orderType
+	postData[`ioc`] = false
 	if accountType == model.AccountTypeReduce {
 		postData[`reduceOnly`] = true
+	} else {
+		postData[`reduceOnly`] = false
 	}
 	if orderType == `limit` || orderType == `market` {
 		postData[`price`], _ = strconv.ParseFloat(orderPrice, 64)
