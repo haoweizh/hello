27e8726958e37d54380890909c46662862aed1f7
张浩伟 <haoweizh@gmail.com>
2021-03-06 10:49:22 +0800

    add ftx carry speed monitoring

--- carry/Carry.go
+++ carry/Carry.go
@@ -364,7 +364,7 @@ func calcCarryOpen(setting *model.Setting, tickPerp, tickRelated *model.BidAsk, 
 	keys, _ := model.AppConfig.GetKeys(setting.Market)
 	localLimit := openValueLimit
 	if len(keys) > 1 && keys[0] != key && setting.Symbol == `BTMX-PERP` {
-		revert = -0.0025
+		revert = -0.0035
 	}
 	if len(keys) > 1 && keys[0] != key && setting.Symbol != `BTMX-PERP` && setting.Symbol != `AMPL-PERP` {
 		setOpen = (1.5 - usdRate) * setOpen
