d4a2653454ce05f2ae375a82cad44125c05aa9d7
张浩伟 <zhanghaowei@edaijia-inc.cn>
2021-04-23 12:48:49 +0800

    test

--- api/WebSocket.go
+++ api/WebSocket.go
@@ -87,7 +87,7 @@ func chanHandler(market string, stopC chan struct{}, errHandler ErrHandler, msgH
 				return
 			}
 			//util.SocketInfo(string(message))
-			msgHandler(message)
+			go msgHandler(message)
 		}
 	}
 }
