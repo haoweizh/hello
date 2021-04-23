5536b73bffdf0ac9f60b80aee4fea815a4867199
张浩伟 <zhanghaowei@edaijia-inc.cn>
2021-04-23 21:53:58 +0800

    test

--- api/Okex.go
+++ api/Okex.go
@@ -281,20 +281,20 @@ func handleBooksUpdate(instrument string, isSpot bool, data map[string]interface
 			//}
 		} else {
 			success = false
-			setWrong(instrument, true)
-			//now := time.Now()
-			//if now.Second() == 0 {
-			//	util.Notice(fmt.Sprintf(`ts %d wrong checksum %s %d %d-%d %v`,
-			//		bidAskUpdate.Ts, instrument, wrongSize, bidAskUpdate.Bids.Len(), bidAskUpdate.Asks.Len(), data))
-			//	if bidAsk.Bids.Len() > 5 && bidAsk.Asks.Len() > 5 {
-			//		util.Notice(fmt.Sprintf(`%v %v %v %v %v - %v %v %v %v %v`,
-			//			bidAskUpdate.Bids[0].Price, bidAskUpdate.Bids[1].Price, bidAskUpdate.Bids[2].Price, bidAskUpdate.Bids[3].Price,
-			//			bidAskUpdate.Bids[4].Price, bidAskUpdate.Asks[0].Price, bidAskUpdate.Asks[1].Price, bidAskUpdate.Asks[2].Price,
-			//			bidAskUpdate.Asks[3].Price, bidAskUpdate.Asks[4].Price))
-			//	} else {
-			//		util.Notice(`>>>>>>>>>>> size too small for ` + instrument)
-			//	}
-			//}
+			wrongSize := setWrong(instrument, true)
+			now := time.Now()
+			if now.Second() == 0 {
+				util.Notice(fmt.Sprintf(`ts %d wrong checksum %s %d %d-%d %v`,
+					bidAskUpdate.Ts, instrument, wrongSize, bidAskUpdate.Bids.Len(), bidAskUpdate.Asks.Len(), data))
+				if bidAsk.Bids.Len() > 5 && bidAsk.Asks.Len() > 5 {
+					util.Notice(fmt.Sprintf(`%v %v %v %v %v - %v %v %v %v %v`,
+						bidAskUpdate.Bids[0].Price, bidAskUpdate.Bids[1].Price, bidAskUpdate.Bids[2].Price, bidAskUpdate.Bids[3].Price,
+						bidAskUpdate.Bids[4].Price, bidAskUpdate.Asks[0].Price, bidAskUpdate.Asks[1].Price, bidAskUpdate.Asks[2].Price,
+						bidAskUpdate.Asks[3].Price, bidAskUpdate.Asks[4].Price))
+				} else {
+					util.Notice(`>>>>>>>>>>> size too small for ` + instrument)
+				}
+			}
 		}
 	}
 	return success, bidAskUpdate
