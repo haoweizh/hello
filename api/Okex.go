03d71112e498a559ba808a63aa773fd80eeb1ea7
张浩伟 <zhanghaowei@edaijia-inc.cn>
2021-04-23 17:58:33 +0800

    test

--- api/Okex.go
+++ api/Okex.go
@@ -252,28 +252,28 @@ func handleBooksUpdate(instrument string, isSpot bool, data map[string]interface
 		compare, _ := data[`checksum`].(json.Number).Int64()
 		bidAskUpdate.Bids = newBids
 		bidAskUpdate.Asks = newAsks
-		//now := time.Now()
+		now := time.Now()
 		if compare == crcValue {
 			success = true
-			//if now.Second() == 0 {
-			//	util.Notice(fmt.Sprintf(`right checksum %s %v %d`, instrument, isSpot, bidAsk.Bids.Len()))
-			//	setWrong(instrument, false)
-			//}
+			if now.Second() == 0 {
+				util.Notice(fmt.Sprintf(`right checksum %s %v %d`, instrument, isSpot, bidAsk.Bids.Len()))
+				setWrong(instrument, false)
+			}
 		} else {
 			success = false
-			setWrong(instrument, true)
-			//if now.Second() == 0 {
-			//	util.Notice(fmt.Sprintf(`ts %d wrong checksum %s %d %d-%d %v`,
-			//		bidAskUpdate.Ts, instrument, wrongSize, bidAskUpdate.Bids.Len(), bidAskUpdate.Asks.Len(), data))
-			//	if bidAsk.Bids.Len() > 5 && bidAsk.Asks.Len() > 5 {
-			//		util.Notice(fmt.Sprintf(`%v %v %v %v %v - %v %v %v %v %v`,
-			//			bidAskUpdate.Bids[0].Price, bidAskUpdate.Bids[1].Price, bidAskUpdate.Bids[2].Price, bidAskUpdate.Bids[3].Price,
-			//			bidAskUpdate.Bids[4].Price, bidAskUpdate.Asks[0].Price, bidAskUpdate.Asks[1].Price, bidAskUpdate.Asks[2].Price,
-			//			bidAskUpdate.Asks[3].Price, bidAskUpdate.Asks[4].Price))
-			//	} else {
-			//		util.Notice(`>>>>>>>>>>> size too small for ` + instrument)
-			//	}
-			//}
+			wrongSize := setWrong(instrument, true)
+			if now.Second() == 0 {
+				util.Notice(fmt.Sprintf(`ts %d wrong checksum %s %d %d-%d %v`,
+					bidAskUpdate.Ts, instrument, wrongSize, bidAskUpdate.Bids.Len(), bidAskUpdate.Asks.Len(), data))
+				if bidAsk.Bids.Len() > 5 && bidAsk.Asks.Len() > 5 {
+					util.Notice(fmt.Sprintf(`%v %v %v %v %v - %v %v %v %v %v`,
+						bidAskUpdate.Bids[0].Price, bidAskUpdate.Bids[1].Price, bidAskUpdate.Bids[2].Price, bidAskUpdate.Bids[3].Price,
+						bidAskUpdate.Bids[4].Price, bidAskUpdate.Asks[0].Price, bidAskUpdate.Asks[1].Price, bidAskUpdate.Asks[2].Price,
+						bidAskUpdate.Asks[3].Price, bidAskUpdate.Asks[4].Price))
+				} else {
+					util.Notice(`>>>>>>>>>>> size too small for ` + instrument)
+				}
+			}
 		}
 	}
 	return success, bidAskUpdate
