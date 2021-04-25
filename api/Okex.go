5914e89ca7574039043a6e6471ec092ad18f130c
张浩伟 <zhanghaowei@edaijia-inc.cn>
2021-04-25 19:33:34 +0800

    release increase tick

--- api/Okex.go
+++ api/Okex.go
@@ -86,7 +86,7 @@ var subscribeHandlerOKEX = func(subscribes []interface{}, subType string) error 
 func handleMsgOKEX(channel chan *simplejson.Json, instrument string) {
 	var responseJson *simplejson.Json
 	for responseJson = range channel {
-		if len(channel) > 20 || time.Now().Second() == 0 {
+		if len(channel) > 20 {
 			util.Notice(fmt.Sprintf(`%s current chan to be handle %d`, instrument, len(channel)))
 		}
 		symbol := model.GetInstrumentSymbol(model.OKEX, instrument)
