cec5add137b47254e729314387acd19ec5425792
张浩伟 <zhanghaowei@edaijia-inc.cn>
2021-05-26 22:39:50 +0800

    binance filter out perp trigger

--- carry/Carry.go
+++ carry/Carry.go
@@ -242,7 +242,7 @@ var ProcessCarry = func(setting *model.Setting, tick *model.BidAsk) {
 		(model.AppConfig.Env != `test` && (model.AppConfig.Handle != `1` || (delayRelated > 300 || delayPerp > 300))) {
 		return
 	}
-	if (setting.Market == model.Binance && (delayPerp > 100 || delayRelated > 100)) ||
+	if (setting.Market == model.Binance && (delayPerp > 100 || delayRelated > 100 || tick == tickPerp)) ||
 		(setting.Market != model.Binance && delayTick > 30) {
 		return
 	}
