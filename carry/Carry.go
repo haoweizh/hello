8f06bf1822fa179a5d07077360554a7ed16e9fba
张浩伟 <zhanghaowei@edaijia-inc.cn>
2021-05-27 13:02:16 +0800

    change ftx filter delay back to 90ms

--- carry/Carry.go
+++ carry/Carry.go
@@ -243,7 +243,7 @@ var ProcessCarry = func(setting *model.Setting, tick *model.BidAsk) {
 		return
 	}
 	if (setting.Market == model.Binance && (delayPerp > 100 || delayRelated > 100 || tick == tickPerp)) ||
-		(setting.Market != model.Binance && delayTick > 30) {
+		(setting.Market == model.OKEX && delayTick > 30) || (setting.Market == model.Ftx && delayTick > 90) {
 		return
 	}
 	scoreOpen := 1 - tickRelated.Asks[0].Price/tickPerp.Bids[0].Price
