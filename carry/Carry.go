d193bb55bb807bee81d2b29f2d14efbcb2281d72
张浩伟 <zhanghaowei@edaijia-inc.cn>
2021-05-28 18:58:09 +0800

    change ftx ticker delay back to 25

--- carry/Carry.go
+++ carry/Carry.go
@@ -243,7 +243,7 @@ var ProcessCarry = func(setting *model.Setting, tick *model.BidAsk) {
 		return
 	}
 	if (setting.Market == model.Binance && (delayPerp > 100 || delayRelated > 100)) ||
-		(setting.Market == model.OKEX && delayTick > 30) || (setting.Market == model.Ftx && delayTick > 90) {
+		(setting.Market == model.OKEX && delayTick > 30) || (setting.Market == model.Ftx && delayTick > 25) {
 		return
 	}
 	scoreOpen := 1 - tickRelated.Asks[0].Price/tickPerp.Bids[0].Price
