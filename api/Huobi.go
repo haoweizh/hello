01c5e749780087b31dec484e440365664932f2d3
sonicwang <393333800@qq.com>
2021-06-06 10:51:29 +0800

    fix huobi future sizeIncrement

--- api/Huobi.go
+++ api/Huobi.go
@@ -244,11 +244,12 @@ func getMarketsHuobi() (marketInfos map[string]*model.MarketInfo) {
 			if value["contract_size"] != nil {
 				marketInfo.CTValue, _ = value["contract_size"].(json.Number).Float64()
 				marketInfo.SizeMin, _ = value["contract_size"].(json.Number).Float64()
+				marketInfo.SizeIncrement, _ = value["contract_size"].(json.Number).Float64()
 			}
 			if value["price_tick"] != nil {
 				marketInfo.PriceIncrement, _ = value["price_tick"].(json.Number).Float64()
 			}
-			marketInfo.SizeIncrement = 1
+
 			marketInfos[marketInfo.Name] = marketInfo
 		}
 	}
