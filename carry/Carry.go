04a0114c2ad918dae0887e648513bf7cab1a3ba1
张浩伟 <zhanghaowei@edaijia-inc.cn>
2021-08-18 13:48:15 +0800

    change gate tick filter

--- carry/Carry.go
+++ carry/Carry.go
@@ -257,16 +257,35 @@ var ProcessCarry = func(setting *model.Setting, tick *model.BidAsk) {
 	if tickRelated != nil {
 		delayRelated = million - int64(tickRelated.Ts)
 	}
+	exit := false
 	if tickPerp == nil || tickRelated == nil || tickPerp.Asks == nil || tickPerp.Bids == nil ||
 		tickRelated.Asks == nil || tickRelated.Bids == nil || setting == nil || model.AppPause ||
 		(model.AppConfig.Env != `test` && model.AppConfig.Handle != `1`) {
-		return
+		exit = true
 	}
-	if (setting.Market == model.Binance && (delayPerp > 100 || delayRelated > 100)) ||
-		(setting.Market == model.OKEX && (delayTick > 25 || delayRelated > 300 || delayPerp > 300)) ||
-		(setting.Market == model.Ftx && delayTick > 95 || delayRelated > 300 || delayPerp > 300) ||
-		//(setting.Market == model.Gate && (delayTick > 40 || delayRelated > 30000 || delayPerp > 30000)) ||
-		(setting.Market == model.Huobi && (delayTick > 25 || delayRelated > 300 || delayPerp > 300)) {
+	switch setting.Market {
+	case model.Binance:
+		if delayPerp > 100 || delayRelated > 100 {
+			exit = true
+		}
+	case model.OKEX:
+		if delayTick > 25 || delayRelated > 300 || delayPerp > 300 {
+			exit = true
+		}
+	case model.Ftx:
+		if delayTick > 95 || delayRelated > 300 || delayPerp > 300 {
+			exit = true
+		}
+	case model.Huobi:
+		if delayTick > 25 || delayRelated > 300 || delayPerp > 300 {
+			exit = true
+		}
+	case model.Gate:
+		if delayTick > 40 || delayRelated > 30000 || delayPerp > 30000 {
+			exit = true
+		}
+	}
+	if exit {
 		return
 	}
 	scoreOpen := 1 - tickRelated.Asks[0].Price/tickPerp.Bids[0].Price
