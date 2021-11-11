006813db4d12d00254623f3d185266ce58449a4e
张浩伟 <zhanghaowei@edaijia-inc.cn>
2021-11-11 14:14:17 +0800

    close revert carry

--- carry/Carry.go
+++ carry/Carry.go
@@ -682,9 +682,9 @@ func calcCarryOpen(setting *model.Setting, tickPerp, tickRelated *model.BidAsk, 
 		setOpen = 1
 	}
 	// 针对第一个key(老板账户)关闭反向开仓
-	if len(keys) > 0 && keys[0] == key {
-		setClose = -1
-	}
+	//if len(keys) > 0 && keys[0] == key {
+	setClose = -1
+	//}
 	if scoreClose < setClose || (balance.Amount > 0 && scoreClose <= -1*revertOpen) {
 		bidAmount = tickPerp.Asks[0].Amount
 		if setting.Market == model.OKEX {
