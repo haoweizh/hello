2e72ad6aebb5c3bf5cdea6ba224225793c8fb3aa
张浩伟 <zhanghaowei@edaijia-inc.cn>
2021-11-16 21:41:11 +0800

    change msg

--- carry/DBHandler.go
+++ carry/DBHandler.go
@@ -80,7 +80,8 @@ func MaintainTransFee(key, secret string) {
 				order := api.QueryOrderById(key, secret, value.Market, value.Symbol, value.Instrument,
 					value.OrderType, value.OrderId)
 				if order == nil {
-					if value.Market == model.Ftx && strings.Contains(value.RefreshType, `carry`) {
+					if value.Market == model.Ftx && (strings.Contains(value.RefreshType, `carry`) ||
+						strings.Contains(value.RefreshType, `comp`)) {
 						value.Status = model.CarryStatusNotWorking
 						model.AppDB.Save(&value)
 					}
