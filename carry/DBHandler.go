50d97a37dfc0796011277f0ebf15f0f42cf0229f
张浩伟 <zhanghaowei@edaijia-inc.cn>
2021-11-16 21:08:06 +0800

    change msg

--- carry/DBHandler.go
+++ carry/DBHandler.go
@@ -68,7 +68,7 @@ func MaintainTransFee(key, secret string) {
 			lastDays2 := now.Add(d)
 			lastMin10 := now.Add(dMin10)
 			model.AppDB.Limit(100).Offset(feeIndex).Where(
-				`date(created_at)>? and date(created_at)<? and status=? and refresh_type!=?`,
+				`created_at>? and created_at<? and status=? and refresh_type!=?`,
 				lastDays2, lastMin10, model.CarryStatusWorking, model.FunctionDCarry).Find(&orders)
 			util.Info(fmt.Sprintf(`--- get working orders %d %v %v`, len(orders), lastDays2, lastMin10))
 			if len(orders) == 0 {
