92835176d305a78e113181233fd0f70a89a5bd6b
张浩伟 <zhanghaowei@edaijia-inc.cn>
2021-12-19 14:40:49 +0800

    change config struct

--- api/Functions.go
+++ api/Functions.go
@@ -40,7 +40,7 @@ func RequireDepthChanReset(markets *model.Markets, market string) bool {
 			return false
 		}
 	}
-	util.Notice(`need reset %s %d %s %d`, now, market, model.AppConfig.Delay)
+	util.Notice(`need reset %d %s %d`, now, market, model.AppConfig.Delay)
 	return true
 }
 
