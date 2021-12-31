a877a494139f63726c63a97b9a19642494114ca8
sonic-win <393333800@qq.com>
2022-01-01 00:25:12 +0800

    postOrderCross handle all status LimitBuy and LimitSell

--- carry/cross/Cross.go
+++ carry/cross/Cross.go
@@ -535,20 +535,22 @@ var postOrderCross = func(order *model.Order, setting *model.Setting) {
 		return
 	}
 	account := model.AppConfig.GetAccountFromKey(order.Market, order.AmountType)
-	if order.HaveId() {
-		if account != nil {
-			_, maxBuy, maxSell := api.GetTradeMaxOKEX(order.AmountType, ``, order.Symbol, -1)
-			if order.OrderSide == model.OrderSideBuy {
-				maxBuy -= order.Amount
-				maxSell += order.Amount
-			} else if order.OrderSide == model.OrderSideSell {
-				maxBuy += order.Amount
-				maxSell -= order.Amount
-			}
-			status := getCarryStatus(setting.Coin, setting.Market, setting.Symbol, account.Key)
-			status.LimitSell = math.Min(status.LimitSell, maxSell)
-			status.LimitBuy = math.Min(status.LimitBuy, maxBuy)
+	if account != nil {
+		status := getCarryStatus(setting.Coin, setting.Market, setting.Symbol, account.Key)
+		maxBuy := status.LimitBuy
+		maxSell := status.LimitSell
+		//_, maxBuy, maxSell := api.GetTradeMaxOKEX(order.AmountType, ``, order.Symbol, -1)
+		if order.OrderSide == model.OrderSideBuy {
+			maxBuy -= order.Amount
+			maxSell += order.Amount
+		} else if order.OrderSide == model.OrderSideSell {
+			maxBuy += order.Amount
+			maxSell -= order.Amount
 		}
+		status.LimitSell = math.Min(status.LimitSell, maxSell)
+		status.LimitBuy = math.Min(status.LimitBuy, maxBuy)
+	}
+	if order.HaveId() {
 		addLastCarry(order, setting)
 		addCarryResult(order.AmountType, order.Market, true)
 	} else {
