170d15e351cf8d0d89fb58d8b17f5eb1d72d2bd2
张浩伟 <zhanghaowei@edaijia-inc.cn>
2022-01-01 21:46:59 +0800

    cross v0.5

--- model/MarketInfo.go
+++ model/MarketInfo.go
@@ -143,19 +143,19 @@ func FormatCrossPair(marketBuy, marketSell, symbolBuy, symbolSell string, amount
 		if marketInfoBuy.CTValue > 0 {
 			incBuy, minBuy = incBuy*marketInfoBuy.CTValue, minBuy*marketInfoBuy.CTValue
 		} else if marketInfoBuy.UsdtMin > 0 && amount*price < marketInfoBuy.UsdtMin {
+			util.Notice(fmt.Sprintf(`marketinfo ctvalue %s %s %f; %f * %f < %f`,
+				marketInfoBuy.Market, marketInfoBuy.Name, marketInfoBuy.CTValue, amount, price, marketInfoBuy.UsdtMin))
 			amount = 0
 		}
-		util.Notice(fmt.Sprintf(`marketinfo ctvalue %f; %f * %f < %f`,
-			marketInfoBuy.CTValue, amount, price, marketInfoBuy.UsdtMin))
 	}
 	if marketInfoSell.CTCurrency == GetCoin(marketSell, symbolSell) {
 		if marketInfoSell.CTValue > 0 {
 			incSell, minSell = incSell*marketInfoSell.CTValue, minSell*marketInfoSell.CTValue
 		} else if marketInfoSell.UsdtMin > 0 && amount*price < marketInfoSell.UsdtMin {
+			util.Notice(fmt.Sprintf(`marketinfo ctvalue %s %s %f; %f * %f < %f`,
+				marketInfoSell.Market, marketInfoSell.Name, marketInfoSell.CTValue, amount, price, marketInfoSell.UsdtMin))
 			amount = 0
 		}
-		util.Notice(fmt.Sprintf(`marketinfo ctvalue %f; %f * %f < %f`,
-			marketInfoSell.CTValue, amount, price, marketInfoSell.UsdtMin))
 	}
 	sizeInc := math.Max(incBuy, incSell)
 	formattedAmount = math.Floor(amount/sizeInc) * sizeInc
