7a9cff503af9dc34aa5929dc918b3eb54d9c142a
王伟 <wangwei19@tuhu.cn>
2022-01-06 16:29:54 +0800

    cross ftx price单位换算成usdt

--- api/Ftx.go
+++ api/Ftx.go
@@ -139,18 +139,24 @@ func handleTickerFtx(markets *model.Markets, response *simplejson.Json) {
 		bidAsk.Asks = []model.Tick{{Price: data.Get(`ask`).MustFloat64(), Amount: data.Get(`askSize`).MustFloat64(),
 			Market: model.Ftx, Symbol: symbol, Side: model.OrderSideSell}}
 	}
+	for function, handler := range model.GetFunctions(model.Ftx, symbol) {
+		if handler != nil {
+			setting := model.GetSetting(function, model.Ftx, symbol)
+			if setting != nil && setting.Function == model.FunctionCross {
+				success, usdtBidAsk := markets.GetBidAsk("USDT/USD", model.Ftx)
+				if success {
+					bidAsk.Asks[0].Price /= usdtBidAsk.Asks[0].Price
+					bidAsk.Bids[0].Price /= usdtBidAsk.Asks[0].Price
+					break
+				}
+			}
+		}
+	}
 	if markets.SetBidAsk(symbol, model.Ftx, bidAsk) {
 		for function, handler := range model.GetFunctions(model.Ftx, symbol) {
 			if handler != nil {
 				setting := model.GetSetting(function, model.Ftx, symbol)
 				if setting != nil {
-					//if setting.Function == model.FunctionCross {
-					//	success, usdtBidAsk := markets.GetBidAsk("USDT/USD", model.Ftx)
-					//	if success {
-					//		bidAsk.Asks[0].Price /= usdtBidAsk.Asks[0].Price
-					//		bidAsk.Bids[0].Price /= usdtBidAsk.Asks[0].Price
-					//	}
-					//}
 					go handler(setting, bidAsk)
 				}
 			}
