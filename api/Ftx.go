c2ec717995d7e9642d8462cd5c585a6aa060bc6e
张浩伟 <zhanghaowei@edaijia-inc.cn>
2022-01-14 19:09:30 +0800

    cross v0.8

--- api/Ftx.go
+++ api/Ftx.go
@@ -789,8 +789,9 @@ func SignedRequestFtx(key, secret, method, path string, param, body map[string]i
 	sign := hex.EncodeToString(hash.Sum(nil))
 	headers := map[string]string{`FTX-KEY`: key, `FTX-TS`: strconv.FormatInt(ts, 10), "FTX-SIGN": sign,
 		"Content-Type": "application/json"}
-	//if key == `aimxyPus258z84JrKahgt--6uWT8tcEeEbyIi8gF` {
-	//	headers[`FTX-SUBACCOUNT`] = `test`
+	if key == `aimxyPus258z84JrKahgt--6uWT8tcEeEbyIi8gF` {
+		headers[`FTX-SUBACCOUNT`] = `test`
+	}
 	//} else if key == `u7oa6J_2ZXq1Putf8swnAKE1MH2T86Zx6JBZoChe` {
 	//	headers[`FTX-SUBACCOUNT`] = `test2`
 	//}
