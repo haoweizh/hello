80d7a7a05cffc7cb12caabcc8dc45085a4c233ca
张浩伟 <zhanghaowei@edaijia-inc.cn>
2022-01-21 15:29:17 +0800

    cross v0.9

--- carry/cross/CrossStatus.go
+++ carry/cross/CrossStatus.go
@@ -22,9 +22,9 @@ var InsufficientCodeOKEX = map[string]bool{`51008`: true, `51119`: true, `51120`
 // market/symbol/bool经过人工确认可以cross的币种
 var validSymbol = map[string]map[string]bool{
 	model.Gate: {`AE_USDT`: true, `HC_USDT`: true, `REEF_PERP`: true, `REEF_USDT`: true,
-		`ONE_PERP`: true, `ONE_USDT`: true},
+		`ONE_PERP`: true, `ONE_USDT`: true, `LSK_USDT`: true},
 	model.OKEX: {`AE-USDT`: true, `HC-USDT`: true, `AE-USDT-SWAP`: true, `HC-USDT-SWAP`: true,
-		`ORBS-USDT`: true, `ORBS-USDT-SWAP`: true, `ONE-USDT`: true},
+		`ORBS-USDT`: true, `ORBS-USDT-SWAP`: true, `ONE-USDT`: true, `LSK-USDT`: true},
 	model.Ftx: {`REEF/USD`: true, `REEF-PERP`: true, `ORBS-PERP`: true, `ORBS/USD`: true,
 						`ONE-PERP`: true}}
 var carryFail = make(map[string]int64) // key fail num
