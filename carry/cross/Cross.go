d967cbf5fcda6aec8cc3d9514c2153d5ee9fdfd8
张浩伟 <zhanghaowei@edaijia-inc.cn>
2022-02-03 12:51:49 +0800

    cross related testing

--- carry/cross/Cross.go
+++ carry/cross/Cross.go
@@ -551,10 +551,6 @@ func calcAmount(index int, coin string, carryStatus, carryStatusRelate *CarrySta
 		initLimitBuyAndSell(statusSell, statusSell.setting, priceSell)
 	}
 	amount = math.Min(math.Min(statusBuy.LimitBuy, bidAmount), math.Min(statusSell.LimitSell, askAmount))
-	if amount <= 0 && (statusBuy.LimitBuy < 0) {
-		util.Notice(`status sell %s %s %f`, statusSell.market, statusSell.symbol, statusSell.LimitSell)
-		util.Notice(`status buy %s %s %f`, statusBuy.market, statusBuy.symbol, statusBuy.LimitBuy)
-	}
 	if amount > 0 {
 		amount = model.FormatCrossPair(statusBuy.market, statusSell.market, statusBuy.symbol, statusSell.symbol, amount, priceBuy)
 	}
