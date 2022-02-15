6dcfcc7e01ccc25da536b4bf5a2f9a4541b93ed2
张浩伟 <zhanghaowei@edaijia-inc.cn>
2022-02-15 18:25:45 +0800

    add controller trade info order

--- controller/Parameter.go
+++ controller/Parameter.go
@@ -181,7 +181,7 @@ func holdPage(c *gin.Context) {
 		}
 	}
 	carryRows, _ := model.AppDB.Model(model.Order{}).Select(`market,amount_type,order_side,sum(price*abs(amount)),date(order_time),refresh_type,count(*)`).
-		Group(`market,order_side,date(order_time),amount_type,refresh_type`).Order(`date(order_time) desc`).Rows()
+		Group(`market,order_side,date(order_time),amount_type,refresh_type`).Order(`date(order_time) desc, market`).Rows()
 	if carryRows != nil {
 		for carryRows.Next() {
 			var marketName, side, date, amountType, refreshType string
