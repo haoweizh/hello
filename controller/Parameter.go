0e54062121f805457651251df710f3d61342861d
张浩伟 <zhanghaowei@edaijia-inc.cn>
2022-03-03 10:37:06 +0800

    controller entry for cancel

--- controller/Parameter.go
+++ controller/Parameter.go
@@ -31,6 +31,7 @@ func ParameterServe() {
 	router.GET(`hold`, holdPage)
 	router.GET(`tick`, tickPage)
 	router.GET(`cross_refresh`, crossRefresh)
+	router.GET(`cancel`, cancel)
 	router.GET(`test`, testSpeed)
 	router.GET(`debug`, debug)
 	router.GET(`wss`, WsPage)
@@ -69,6 +70,21 @@ func WsPage(c *gin.Context) {
 	wsClient.Manager.Register <- wsClient
 	go wsClient.Read(wsHandler)
 	go wsClient.Write()
+}
+
+func cancel(c *gin.Context) {
+	market := c.Query(`market`)
+	symbols := model.GetMarketSymbols(market)
+	accounts := model.AppConfig.GetAccounts(market)
+	for symbol := range symbols {
+		for _, account := range accounts {
+			api.CancelOrders(account.Key, account.Secret, market, symbol)
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": 0,
+		"msg":  fmt.Sprintf(`cancel %s orders accounts %d symbols %d`, market, len(accounts), len(symbols)),
+	})
 }
 
 func debug(c *gin.Context) {
