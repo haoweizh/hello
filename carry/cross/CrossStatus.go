7ee37876f04ab3ac594d7aff52ff90c20e1fa075
张浩伟 <zhanghaowei@edaijia-inc.cn>
2022-03-04 11:01:46 +0800

    add coin TORN

--- carry/cross/CrossStatus.go
+++ carry/cross/CrossStatus.go
@@ -21,9 +21,9 @@ var InsufficientCodeOKEX = map[string]bool{`51008`: true, `51119`: true, `51120`
 	`58350`: true, `59108`: true, `59200`: true}
 
 // market/symbol/bool经过人工确认可以cross的币种
-var validCrossCoin = map[string][]string{
+var validCrossCoin = map[string][]string{model.BinanceSpot: {`TORN`},
 										model.Gate: {`AE`, `HC`, `REEF`, `ONE`, `LSK`, `GLMR`, `LEASH`, `KDA`, `BLOK`},
-										model.OKEX: {`AE`, `HC`, `ORBS`, `ONE`, `LSK`, `GLMR`, `LEASH`, `KLAY`, `KDA`, `BLOK`},
+										model.OKEX: {`AE`, `HC`, `ORBS`, `ONE`, `LSK`, `GLMR`, `LEASH`, `KLAY`, `KDA`, `BLOK`, `TORN`},
 										model.Ftx:  {`REEF`, `ORBS`, `ONE`}, model.BybitPerp: {`KLAY`}}
 var lastOrderIndex = make(map[string]map[string]int64)                        // market - symbol - index
 var lastOrders = make(map[string]map[string][]*model.Order, lastOrderLength)  // market - symbol - []order
