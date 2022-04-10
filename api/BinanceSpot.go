4996fda6f0d97898e950411530b8b06fc0e14616
张浩伟 <zhanghaowei@edaijia-inc.cn>
2022-04-10 23:39:31 +0800

    prepare for mexc

--- api/BinanceSpot.go
+++ api/BinanceSpot.go
@@ -260,7 +260,7 @@ func placeOrderBinanceSpot(key, secret string, order *model.Order, orderSide, or
 		}
 		orderResponse, err := service.Do(context.Background())
 		if err != nil {
-			util.Notice("placeOrderBinanceSpot err: " + err.Error())
+			util.Notice(fmt.Sprintf(`placeOrderBinanceSpot err: %s amount %s`, err.Error(), amountStr))
 			order.OrderId = ``
 		} else {
 			order.OrderId = strconv.FormatInt(orderResponse.OrderID, 10)
