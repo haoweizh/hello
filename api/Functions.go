8a1bbf0b35143b603ba3c419b087294dcb6ffe02
张浩伟 <zhanghaowei@edaijia-inc.cn>
2022-05-09 12:10:00 +0800

    update filter coins

--- api/Functions.go
+++ api/Functions.go
@@ -681,13 +681,15 @@ func GetMarketInfos(market string) (marketInfo map[string]*model.MarketInfo) {
 // 搬砖过滤币种 `AMPL`, `IOTA` REEF
 // 某些主流币 BTC ETH LINK
 // 币种对不上 REAL, DFL, QI, WSB, TRADE,FAME,BIFI,TON,BOX,PAY
-// 法币 GBP CUSDT `TRYB``BRZ``CAD``EUR` `SUSD` `USDC` `TUSD`USDT
+// 法币 GBP CUSDT `TRYB``BRZ``CAD``EUR` `SUSD` `USDC` `TUSD`USDT EURT
 // 平台币 `GT` `FTT` `BNB` `OKB` MX
 // ftx预测`TRUMP``BOLSONARO`
 func filterCross(market, symbol string) bool {
 	filterCoins := map[string]bool{`AMPL`: true, `IOTA`: true, `REEF`: true, `BTC`: true, `ETH`: true, `LINK`: true,
-		`REAL`: true, `DFL`: true, `QI`: true, `WSB`: true, `TRADE`: true, `FAME`: true, `BIFI`: true, `TON`: true, `BOX`: true, `PAY`: true,
-		`GBP`: true, `CUSDT`: true, `TRYB`: true, `BRZ`: true, `CAD`: true, `EUR`: true, `SUSD`: true, `USDC`: true, `TUSD`: true, `USDT`: true,
+		`REAL`: true, `DFL`: true, `QI`: true, `WSB`: true, `TRADE`: true, `FAME`: true, `BIFI`: true, `TON`: true,
+		`BOX`: true, `PAY`: true, `GTC`: true,
+		`GBP`: true, `CUSDT`: true, `TRYB`: true, `BRZ`: true, `CAD`: true, `EUR`: true, `SUSD`: true, `USDC`: true,
+		`TUSD`: true, `USDT`: true, `EURT`: true,
 		`GT`: true, `FTT`: true, `BNB`: true, `OKB`: true, `MX`: true, `TRUMP`: true, `BOLSONARO`: true, `DEFI`: true}
 	_, _, coin, _ := model.GetFromStandard(market, symbol)
 	if filterCoins[coin] {
