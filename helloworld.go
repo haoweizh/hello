f0753d024f939bb4334e572b359bb90e187c10b5
张浩伟 <zhanghaowei@edaijia-inc.cn>
2022-05-11 22:11:33 +0800

    add pprof

--- helloworld.go
+++ helloworld.go
@@ -8,16 +8,17 @@ import (
 	"hello/controller"
 	"hello/model"
 	"hello/util"
-	//_ "net/http/pprof"
+	"net/http"
+	_ "net/http/pprof"
 )
 
 func main() {
-	//go func() {
-	//	err := http.ListenAndServe("0.0.0.0:8081", nil)
-	//	if err != nil {
-	//		return
-	//	}
-	//}()
+	go func() {
+		err := http.ListenAndServe("0.0.0.0:8081", nil)
+		if err != nil {
+			return
+		}
+	}()
 	model.NewConfig()
 	var err error
 	model.AppDB, err = gorm.Open(postgres.Open(model.AppConfig.DBConnection), &gorm.Config{})
