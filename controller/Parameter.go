679cd62603a5458a531aa0a1c0c238caffe8a47f
张浩伟 <zhanghaowei@edaijia-inc.cn>
2022-05-11 21:20:41 +0800

    add pprof

--- controller/Parameter.go
+++ controller/Parameter.go
@@ -26,8 +26,7 @@ func ParameterServe() {
 	router := gin.Default()
 	_ = router.SetTrustedProxies(nil)
 	router.LoadHTMLGlob("templates/*")
-	router.GET(`/`, empty)
-	router.GET(`turtle`, GetParameters)
+	router.GET("/", GetParameters)
 	router.GET(`refresh`, RefreshParameters)
 	router.GET(`pw`, GetCode)
 	router.GET(`cross`, crossPage)
@@ -47,10 +46,6 @@ func ParameterServe() {
 		fmt.Println(`port occupied, exit ` + err.Error())
 		os.Exit(1)
 	}
-}
-
-func empty(c *gin.Context) {
-	c.String(http.StatusOK, fmt.Sprintf(`success`))
 }
 
 func WsPage(c *gin.Context) {
--- helloworld.go
+++ helloworld.go
@@ -8,9 +8,15 @@ import (
 	"hello/controller"
 	"hello/model"
 	"hello/util"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
 )
 
 func main() {
+	go func() {
+		log.Println(http.ListenAndServe("localhost:8081", nil))
+	}()
 	model.NewConfig()
 	var err error
 	model.AppDB, err = gorm.Open(postgres.Open(model.AppConfig.DBConnection), &gorm.Config{})
