324607a34f735a0c191647a980aa6d60acc2a092
张浩伟 <zhanghaowei@edaijia-inc.cn>
2022-05-24 20:25:54 +0800

    add log check trade line

--- carry/cross/Cross.go
+++ carry/cross/Cross.go
@@ -259,6 +259,14 @@ func initStatus(account *model.Account, setting *model.Setting) (status *CarrySt
 	}
 	status.TradeLineBuy = math.Max(standardScoreBuy*(0.5+jumpBuy*status.RateInAll), lowestScore) + fundingRate
 	status.TradeLineSell = math.Max(standardScoreSell*(0.5+jumpSell*status.RateInAll), lowestScore) - fundingRate
+	if status.setting.Coin == `ANC` && account.Index == 0 {
+		util.Info(fmt.Sprintf(`%s %s bline %f = max(%f*(0.5+%f*rate %f)+funding %f hold %f`,
+			status.setting.Market, status.setting.Symbol, status.TradeLineBuy, standardScoreBuy, jumpBuy,
+			status.RateInAll, fundingRate, status.Holding))
+		util.Info(fmt.Sprintf(`%s %s sline %f = max(%f*(0.5+%f*rate %f)-funding %f hold %f`,
+			status.setting.Market, status.setting.Symbol, status.TradeLineSell, standardScoreSell, jumpSell,
+			status.RateInAll, fundingRate, status.Holding))
+	}
 	status.TradeLineBuy *= account.CarryRate
 	status.TradeLineSell *= account.CarryRate
 	if doRevert || account.CarryClose {
