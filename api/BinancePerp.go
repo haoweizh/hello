7c546346641ae8de731d10168d6b6047577a54ea
张浩伟 <zhanghaowei@edaijia-inc.cn>
2022-08-16 13:53:09 +0800

    修改ok order amount设置的精度

--- api/BinancePerp.go
+++ api/BinancePerp.go
@@ -277,7 +277,8 @@ func placeOrderBinancePerp(key, secret string, order *model.Order, orderSide, or
 			order.OrderId = ``
 			order.ErrCode = err.Error()
 		} else {
-			order.Amount, _ = strconv.ParseFloat(orderResponse.OrigQuantity, 64)
+			orderedAmount, _ := strconv.ParseFloat(orderResponse.OrigQuantity, 64)
+			model.ParseRealAmount(model.BinancePerp, symbol, orderedAmount)
 			order.OrderId = strconv.FormatInt(orderResponse.OrderID, 10)
 		}
 	}
