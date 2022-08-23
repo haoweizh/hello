ec3285cafdf6e666ec21fc807c067d2ff27ff21f
张浩伟 <zhanghaowei@edaijia-inc.cn>
2022-08-23 13:58:43 +0800

    修改turtle赋值order给turtle的过滤

--- carry/Turtle.go
+++ carry/Turtle.go
@@ -177,17 +177,11 @@ func checkTurtleOrders(key, secret string, setting *model.Setting, currentN floa
 			return
 		}
 		for _, order := range orders {
-			if turtleData.orderLong != nil && turtleData.orderLong.OrderId == order.OrderId {
-				turtleData.orderLong = order
-				if currentN < setting.AmountLimit || setting.Chance < 0 {
-					continue
-				}
-			}
-			if turtleData.orderShort != nil && turtleData.orderShort.OrderId == order.OrderId {
-				turtleData.orderShort = order
-				if currentN > -1*setting.AmountLimit || setting.Chance > 0 {
-					continue
-				}
+			if (turtleData.orderLong != nil && turtleData.orderLong.OrderId == order.OrderId &&
+				(currentN < setting.AmountLimit || setting.Chance < 0)) ||
+				(turtleData.orderShort != nil && turtleData.orderShort.OrderId == order.OrderId &&
+					currentN > -1*setting.AmountLimit || setting.Chance > 0) {
+				continue
 			}
 			result := api.MustCancel(key, secret, setting.Market, setting.Symbol, order.OrderType, order.OrderId, true)
 			util.Notice(`cancel extra turtle order %s %s %s %s return %v`,
