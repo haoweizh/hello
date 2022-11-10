0e46a365ac72c7f00ffd3cb270528245749708b9
张浩伟 <zhanghaowei@edaijia-inc.cn>
2022-11-10 18:15:30 +0800

    update cross move

--- api/Functions.go
+++ api/Functions.go
@@ -825,16 +825,20 @@ func InitCrossMarketInfos(markets []string) {
 	model.AppDB.Model(&settingsDb).Where(`function=?`, model.FunctionCross).Updates(map[string]interface{}{`valid`: false})
 	for coin, infos := range infoPool {
 		util.Notice(`handle coin %s %d`, coin, len(infos))
+		score := 0.0
 		if len(infos) >= 2 {
 			for _, info := range infos {
+				if info.Market == model.Ftx {
+					score = 0.2
+				}
 				if settingsDbMap[fmt.Sprintf(`%s_%s_%s`, model.FunctionCross, info.Market, info.Name)] == nil {
 					setting := &model.Setting{Valid: true, Function: model.FunctionCross, Market: info.Market,
-						Symbol: info.Name, Coin: coin}
+						Symbol: info.Name, Coin: coin, OpenShortMargin: score}
 					util.Notice(fmt.Sprintf(`save setting %s %s %s %v`, info.Market, info.Name, coin, setting.Valid))
 					model.AppDB.Save(setting)
 				} else {
 					model.AppDB.Model(&settingsDb).Where("market= ? and symbol= ? and function= ?",
-						info.Market, info.Name, model.FunctionCross).Updates(map[string]interface{}{`valid`: true})
+						info.Market, info.Name, model.FunctionCross).Updates(map[string]interface{}{`valid`: true, `open_short_margin`: score})
 				}
 			}
 		}
