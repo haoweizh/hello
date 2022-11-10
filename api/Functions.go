b0181e479c7e7198fd3862d4183df42324f405f1
张浩伟 <zhanghaowei@edaijia-inc.cn>
2022-11-10 08:23:39 +0800

    update cross

--- api/Functions.go
+++ api/Functions.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"hello/model"
 	"hello/util"
+	"math"
 	"strconv"
 	"strings"
 	"sync"
@@ -353,19 +354,27 @@ func GetFundingRate(key, secret, market, symbol string) (success bool, rate floa
 	if fundingRate == nil || now > fundingRate.ExpireTime {
 		return false, 0, util.GetNow()
 	}
-	fRate := fundingRate.Rate * 100
-	fRateNext := fundingRate.RateNext * 100
-	if fRate >= 0 {
-		fundingRate.Rate = fRate * (1 + fRate) / 100
+	if math.Abs(fundingRate.Rate) > 0.02 {
+		fundingRate.Rate = 3 * fundingRate.Rate
 	} else {
-		fRate = -1 * fRate
-		fundingRate.Rate = -1 * fRate * (1 + fRate) / 100
+		fRate := fundingRate.Rate * 100
+		if fRate >= 0 {
+			fundingRate.Rate = fRate * (1 + fRate) / 100
+		} else {
+			fRate = -1 * fRate
+			fundingRate.Rate = -1 * fRate * (1 + fRate) / 100
+		}
 	}
-	if fRateNext > 0 {
-		fundingRate.RateNext = fRateNext * (1 + fRateNext) / 100
+	if math.Abs(fundingRate.RateNext) > 0.02 {
+		fundingRate.RateNext = 3 * fundingRate.RateNext
 	} else {
-		fRateNext = -1 * fRateNext
-		fundingRate.RateNext = -1 * fRateNext * (1 + fRateNext) / 100
+		fRateNext := fundingRate.RateNext * 100
+		if fRateNext > 0 {
+			fundingRate.RateNext = fRateNext * (1 + fRateNext) / 100
+		} else {
+			fRateNext = -1 * fRateNext
+			fundingRate.RateNext = -1 * fRateNext * (1 + fRateNext) / 100
+		}
 	}
 	model.SetFundingRate(market, symbol, fundingRate)
 	return true, fundingRate.Rate, fundingRate.UpdateTime
