328ca165648c2c0f9cd1a4e2875b2596378164bf
张浩伟 <zhanghaowei@edaijia-inc.cn>
2023-01-12 10:44:06 +0800

    update simulation parameter

--- controller/Parameter.go
+++ controller/Parameter.go
@@ -114,6 +114,9 @@ func simulate(c *gin.Context) {
 	}
 	simType := c.Query(`type`)
 	simTypeSeconds, simTypeErr := strconv.ParseInt(simType, 10, 64)
+	if simTypeErr != nil {
+		simTypeSeconds = 86400
+	}
 	coins := strings.Split(c.Query(`coin`), `,`)
 	strBegin := c.Query(`begin`) + `T00:00:00+00:00`
 	strEnd := c.Query(`end`) + `T00:00:00+00:00`
@@ -135,13 +138,13 @@ func simulate(c *gin.Context) {
 	if sessionValue == nil || sessionValue.(string) != code || code == `` {
 		strNew = `false`
 	}
-	if errBegin != nil || errEnd != nil || simTypeErr != nil || simTypeSeconds <= 0 ||
+	if errBegin != nil || errEnd != nil || simTypeSeconds <= 0 ||
 		(simTypeSeconds != 1800 && simTypeSeconds != 14400 && simTypeSeconds%86400 != 0) {
 		simulateGuide := "limit:仓数上限，可选，默认为3 new:true为生成新的仿真否则为查看同参数历史仿真 " +
-			"type:以秒计算单周期长度，取值范围：3600、14400或86400的倍数\n" +
+			"type:海龟的计算周期，默认86400秒，即一天，取值范围：3600、14400或86400的倍数\nmarket:模拟市场\n" +
 			"参数样例：\ncoin=xrp&begin=2022-10-01&end=2022-10-10&limit=3&type=3600&new=true\n"
 		c.String(http.StatusMethodNotAllowed,
-			fmt.Sprintf(`time parameter error %s %s %s\n%s`, strBegin, strEnd, simType, simulateGuide))
+			fmt.Sprintf("time parameter error %s %s %s\n%s", strBegin, strEnd, simType, simulateGuide))
 		return
 	}
 	duration, _ := time.ParseDuration(`10000h`)
