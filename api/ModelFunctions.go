bb601b9cfa241ee11836b49e5272847b6464f8c0
张浩伟 <zhanghaowei@edaijia-inc.cn>
2023-02-02 00:01:31 +0800

    update dynamic top turtle symbol length to 20

--- api/ModelFunctions.go
+++ api/ModelFunctions.go
@@ -114,7 +114,7 @@ func GetFunctions(market, symbol string) *sync.Map {
 	return nil
 }
 
-const topMarketInfoLen = 15
+const topMarketInfoLen = 20
 
 func prepareSettings() {
 	localSymbolSettings := &sync.Map{}
