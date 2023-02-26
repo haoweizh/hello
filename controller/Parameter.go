0352610b1bc75ba41aa16c13487b68a05c88a655
张浩伟 <zhanghaowei@edaijia-inc.cn>
2023-02-26 09:44:58 +0800

    gxzq cut and generate report

--- controller/Parameter.go
+++ controller/Parameter.go
@@ -129,11 +129,11 @@ func simulateGXZQ(c *gin.Context) {
 	coins := `DOGE,SOL,MATIC,CHZ,LINK,ADA,BNB,FIL,SUSHI,AXS,ATOM,WAVES`
 	for i := 3; i <= 21; i++ {
 		sign := fmt.Sprintf(`market%s,coins%s,seconds86400,%s~%s,far%d,near%d,limit%d,allLimit%d,useNear%v`,
-			market, coins, strBegin, strEnd, i*2, i, 3, 912, true)
+			market, coins, strBegin, strEnd, i*2, i, 3, -1, true)
 		regret.CutTail(market, coins, sign)
 		util.Notice(`done cut tail %s %s %s`, market, coins, sign)
 		sign = fmt.Sprintf(`market%s,coins%s,seconds86400,%s~%s,far%d,near%d,limit%d,allLimit%d,useNear%v`,
-			market, coins, strBegin, strEnd, i*2, i, 3, 912, false)
+			market, coins, strBegin, strEnd, i*2, i, 3, -1, false)
 		regret.CutTail(market, coins, sign)
 		util.Notice(`done cut tail %s %s %s`, market, coins, sign)
 	}
