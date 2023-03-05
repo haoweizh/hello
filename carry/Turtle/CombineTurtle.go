701fef5f41100c2f2fe1276118218211b8c9aa20
张浩伟 <zhanghaowei@edaijia-inc.cn>
2023-03-05 20:02:23 +0800

    optimize turtle for combine turtle

--- carry/Turtle/CombineTurtle.go
+++ carry/Turtle/CombineTurtle.go
@@ -123,8 +123,8 @@ var ProcessCombineTurtle = func(settingLimit *model.Setting, tick *model.BidAsk)
 func handleBreakLong(setting, settingOpposite *model.Setting, data, dataOpposite *api.TurtleData,
 	turtleCoins float64, big bool) (work bool) {
 	if data == nil || data.OrderLong == nil || len(data.OrderLong) == 0 || !data.WaitBreakLong || !data.BreakLong {
-		util.Notice(fmt.Sprintf(`handleBreakLong %s %s %s %d %v %v %v`,
-			setting.Market, setting.Symbol, setting.Function, len(data.OrderShort), len(data.OrderShort) == 0, !data.WaitBreakShort, !data.BreakShort))
+		util.Notice(fmt.Sprintf(`handleBreakLong%s %s %s  %v %d %v %v %v`,
+			setting.Market, setting.Symbol, setting.Function, data, len(data.OrderShort), len(data.OrderShort) == 0, !data.WaitBreakShort, !data.BreakShort))
 		return false
 	}
 	data.WaitBreakLong = false
