bdfc32302e5af43a65ec2e9cd0317a1f2c2bb241
张浩伟 <zhanghaowei@edaijia-inc.cn>
2023-03-05 16:53:06 +0800

    optimize turtle for combine turtle

--- api/ModelFunctions.go
+++ api/ModelFunctions.go
@@ -289,6 +289,8 @@ func handleSettings() (handled bool) {
 				turtleData := GetTurtleData(accounts[0].Key, accounts[0].Secret, model.FunctionTurtle, market, marketInfoArray[i].Name)
 				if turtleData != nil {
 					topMarketInfos[marketInfoArray[i].Name] = marketInfoArray[i]
+				} else {
+					util.Notice(fmt.Sprintf(`fail to get top turtle data %s %s`, market, marketInfoArray[i].Name))
 				}
 			}
 		}
