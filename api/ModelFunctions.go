91becb4912d1186155e0ab316a109e2da85777f8
张浩伟 <zhanghaowei@edaijia-inc.cn>
2023-03-06 17:00:43 +0800

    在combine时不再使用turtle标识的setting，改为normal标识的

--- api/ModelFunctions.go
+++ api/ModelFunctions.go
@@ -144,9 +144,12 @@ func prepareSettings() {
 			setting.Market, setting.Symbol, setting.Function, setting.Valid, setting.Chance))
 		marketMap[setting.Market] = true
 		value, ok = util.LoadSyncMap(localHandlers, setting.Market, setting.Symbol)
-		functions := &sync.Map{}
-		if ok && value != nil {
+		var functions *sync.Map
+		if ok {
 			functions = value.(*sync.Map)
+		}
+		if functions == nil {
+			functions = &sync.Map{}
 		}
 		functions.Store(setting.Function, model.HandlerMap[setting.Function])
 		util.StoreSyncMap(localHandlers, functions, setting.Market, setting.Symbol)
