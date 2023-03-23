c6e63faeb69efe33d025ca512303ee4e8d5a7c12
wangwei <393333800@qq.com>
2023-03-23 14:52:28 +0800

    gate perp SizeIncrement=OrderSizeMin

--- api/Gate.go
+++ api/Gate.go
@@ -60,9 +60,10 @@ func appendFutureMarketGate(key, secret string, marketInfos map[string]*model.Ma
 		marketInfo.PriceIncrement = minPrice
 		marketInfo.PriceDecimal = util.NumDecPlaces(minPrice)
 		//marketInfo.SizeMin = float64(contract.OrderSizeMin)
+		//marketInfo.SizeIncrement = marketInfo.SizeMin
 		marketInfo.SizeMin = 10
+		marketInfo.SizeIncrement = float64(contract.OrderSizeMin)
 		marketInfo.SizeMax = float64(contract.OrderSizeMax)
-		marketInfo.SizeIncrement = marketInfo.SizeMin
 		marketInfo.CTCurrency = coin
 		marketInfo.CTValue, _ = strconv.ParseFloat(contract.QuantoMultiplier, 64)
 		marketInfo.BuyLimitPriceRatio, _ = strconv.ParseFloat(contract.OrderPriceDeviate, 64)
