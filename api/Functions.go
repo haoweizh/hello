2582bc2dfd0844106bf90c4b5c13b438ff624529
张浩伟 <zhanghaowei@edaijia-inc.cn>
2023-04-28 21:10:19 +0800

    update parameter open short margin

--- api/Functions.go
+++ api/Functions.go
@@ -875,7 +875,7 @@ func InitCrossMarketInfos(markets []string) {
 	model.AppDB.Model(&settingsDb).Where(`function=?`, model.FunctionCross).Updates(map[string]interface{}{`valid`: false})
 	for coin, infos := range infoPool {
 		//util.Notice(`handle coin %s %d`, coin, len(infos))
-		scoreOpen := 0.02
+		scoreOpen := 0.015
 		scoreClose := 0.005
 		if len(infos) >= 2 {
 			for _, info := range infos {
