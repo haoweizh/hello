1b89f911bd5b5e5e08e4abb3601b7dfd048e88ce
张浩伟 <zhanghaowei@edaijia-inc.cn>
2023-05-31 11:12:02 +0800

    recovery common coins

--- model/MarketInfo.go
+++ model/MarketInfo.go
@@ -11,7 +11,7 @@ import (
 var MarketInfos = &sync.Map{} // market - symbol - *MarketInfo
 
 var CommonCoins = map[string]bool{`btc`: true, `eth`: true, `ltc`: true, `bch`: true, `eos`: true, `xrp`: true,
-	`usdt`: true, `etc`: true, `link`: true}
+	`usdt`: true, `etc`: true, `link`: true, `trx`: true}
 
 type MarketInfo struct {
 	Market, Name, CTCurrency                string
