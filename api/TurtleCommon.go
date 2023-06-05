83847c948e45f1e5df2856e67c8ffe2bdb21adb6
张浩伟 <zhanghaowei@edaijia-inc.cn>
2023-06-05 10:39:30 +0800

    fix turtle n-volume calc issue

--- api/TurtleCommon.go
+++ api/TurtleCommon.go
@@ -387,9 +387,9 @@ func getTurtleCandles(account *model.Account, market, symbol string, far, second
 	} else {
 		lastCandle := sortedCandles.Value[len(sortedCandles.Value)-1]
 		lastCandle.N = (lastCandle2.N*float64(calcLen-1) + lastCandle.PriceHigh - lastCandle.PriceLow) / float64(calcLen)
-		lastCandle.NVolume = (lastCandle2.NVolume*float64(calcLen-1) + lastCandle.NVolume) / float64(calcLen)
-		util.Notice(fmt.Sprintf(`base on last 2 candle %s %s far %d %d n-n %f %f nv-nv %f %f`,
-			market, symbol, far, seconds, lastCandle2.N, lastCandle.N, lastCandle2.NVolume, lastCandle.NVolume))
+		lastCandle.NVolume = (lastCandle2.NVolume*float64(calcLen-1) + lastCandle.Volume) / float64(calcLen)
+		util.Notice(fmt.Sprintf(`base on last 2 candle %s %s far %d %d n-n %f %f nv-nv %f %f len %d`,
+			market, symbol, far, seconds, lastCandle2.N, lastCandle.N, lastCandle2.NVolume, lastCandle.NVolume, calcLen))
 	}
 	return sortedCandles.Value
 }
