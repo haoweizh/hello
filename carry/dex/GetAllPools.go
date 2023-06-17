d2a120ef848be76492e1dccf80f6605f4e533e47
liurongdong <liurongdong@edaijia-inc.cn>
2023-06-17 19:53:52 +0800

    get all pools

--- carry/dex/GetAllPools.go
+++ carry/dex/GetAllPools.go
@@ -7,12 +7,15 @@ import (
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"golang.org/x/crypto/sha3"
 	"log"
 	"math/big"
 	"strconv"
 	"strings"
+	"sync"
+	"time"
 )
 
 var (
@@ -20,10 +23,13 @@ var (
 	mainnetRpcHttps = "https://eth-mainnet.g.alchemy.com/v2/p6QKOpJrOhTeRZ7OT1ufLKVCsqEoKzMG"
 )
 
+var wg sync.WaitGroup
+
 type DexFactory struct {
-	FactoryAddress common.Address // 工厂合约地址
-	StartBlock     *big.Int       // 合约部署的起始区块
-	Abi            string         // 合约ABI
+	FactoryAddress       common.Address // 工厂合约地址
+	StartBlock           *big.Int       // 合约部署的起始区块
+	Abi                  string         // 合约ABI
+	PoolCreateMethodName string         // 创建交易对的方法名
 }
 
 type Dex struct {
@@ -36,29 +42,86 @@ type Pool struct {
 	// TODO: 交易对的信息
 }
 
+var precessGetPairFromLogMap = map[string]func(vLog types.Log, dex Dex){
+	"UniswapV2": processUniswapV2PairFromLog,
+	"UniswapV3": processUniswapV3PairFromLog,
+}
+
+func processUniswapV3PairFromLog(vLog types.Log, dex Dex) {
+
+	fmt.Println(dex.Name)
+	//	解析日志事件
+
+	contractAbi, err := abi.JSON(strings.NewReader(dex.Factory.Abi))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data, err := contractAbi.Unpack(dex.Factory.PoolCreateMethodName, vLog.Data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("univ3poolAddress:", data[0]) // pair address
+
+	var topics [4]string
+	for i := range vLog.Topics {
+		topics[i] = vLog.Topics[i].Hex()
+	}
+	fmt.Println(topics[0]) // 0xe79e73da417710ae99aa2088575580a60415d359acfad9cdd3382d59c80281d4
+}
+
+func processUniswapV2PairFromLog(vLog types.Log, dex Dex) {
+	fmt.Println(dex.Name)
+	//	解析日志事件
+	contractAbi, err := abi.JSON(strings.NewReader(dex.Factory.Abi))
+	if err != nil {
+		log.Fatal(err)
+	}
+	data, err := contractAbi.Unpack(dex.Factory.PoolCreateMethodName, vLog.Data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("合约地址：", data[0]) // pair address
+
+	var topics [4]string
+	for i := range vLog.Topics {
+		topics[i] = vLog.Topics[i].Hex()
+	}
+	fmt.Println("token0地址:", topics[0]) // 0xe79e73da417710ae99aa2088575580a60415d359acfad9cdd3382d59c80281d4
+	fmt.Println("token1地址:", topics[1]) // 0xe79e73da417710ae99aa2088575580a60415d359acfad9cdd3382d59c80281d4
+}
+
 func initDexs() []Dex {
+
 	dexs := []Dex{
-		{
-			Name: "UniswapV2",
-			Factory: DexFactory{
-				FactoryAddress: common.HexToAddress(ToChecksumAddress("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f")),
-				StartBlock:     big.NewInt(0),
-				Abi:            "[{\"inputs\":[{\"internalType\":\"address\",\"name\":\"_feeToSetter\",\"type\":\"address\"}],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"constructor\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"token0\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"address\",\"name\":\"token1\",\"type\":\"address\"},{\"indexed\":false,\"internalType\":\"address\",\"name\":\"pair\",\"type\":\"address\"},{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"name\":\"PairCreated\",\"type\":\"event\"},{\"constant\":true,\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"name\":\"allPairs\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"allPairsLength\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"internalType\":\"address\",\"name\":\"tokenA\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"tokenB\",\"type\":\"address\"}],\"name\":\"createPair\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"pair\",\"type\":\"address\"}],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"feeTo\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"feeToSetter\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"name\":\"getPair\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"internalType\":\"address\",\"name\":\"_feeTo\",\"type\":\"address\"}],\"name\":\"setFeeTo\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"internalType\":\"address\",\"name\":\"_feeToSetter\",\"type\":\"address\"}],\"name\":\"setFeeToSetter\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]",
-			},
-		},
+		//{
+		//	Name: "UniswapV2",
+		//	Factory: DexFactory{
+		//		FactoryAddress:       common.HexToAddress(ToChecksumAddress("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f")),
+		//		StartBlock:           big.NewInt(10000835),
+		//		PoolCreateMethodName: "PairCreated",
+		//		Abi:                  "[{\"inputs\":[{\"internalType\":\"address\",\"name\":\"_feeToSetter\",\"type\":\"address\"}],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"constructor\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"token0\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"address\",\"name\":\"token1\",\"type\":\"address\"},{\"indexed\":false,\"internalType\":\"address\",\"name\":\"pair\",\"type\":\"address\"},{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"name\":\"PairCreated\",\"type\":\"event\"},{\"constant\":true,\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"name\":\"allPairs\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"allPairsLength\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"internalType\":\"address\",\"name\":\"tokenA\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"tokenB\",\"type\":\"address\"}],\"name\":\"createPair\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"pair\",\"type\":\"address\"}],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"feeTo\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"feeToSetter\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"name\":\"getPair\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"internalType\":\"address\",\"name\":\"_feeTo\",\"type\":\"address\"}],\"name\":\"setFeeTo\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"internalType\":\"address\",\"name\":\"_feeToSetter\",\"type\":\"address\"}],\"name\":\"setFeeToSetter\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]",
+		//	},
+		//},
 		{
 			Name: "UniswapV3",
 			Factory: DexFactory{
-				FactoryAddress: common.HexToAddress(ToChecksumAddress("0x1F98431c8aD98523631AE4a59f267346ea31F984")),
-				StartBlock:     big.NewInt(0),
-				Abi:            "[{\"inputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"constructor\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"uint24\",\"name\":\"fee\",\"type\":\"uint24\"},{\"indexed\":true,\"internalType\":\"int24\",\"name\":\"tickSpacing\",\"type\":\"int24\"}],\"name\":\"FeeAmountEnabled\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"oldOwner\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"address\",\"name\":\"newOwner\",\"type\":\"address\"}],\"name\":\"OwnerChanged\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"token0\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"address\",\"name\":\"token1\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"uint24\",\"name\":\"fee\",\"type\":\"uint24\"},{\"indexed\":false,\"internalType\":\"int24\",\"name\":\"tickSpacing\",\"type\":\"int24\"},{\"indexed\":false,\"internalType\":\"address\",\"name\":\"pool\",\"type\":\"address\"}],\"name\":\"PoolCreated\",\"type\":\"event\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"tokenA\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"tokenB\",\"type\":\"address\"},{\"internalType\":\"uint24\",\"name\":\"fee\",\"type\":\"uint24\"}],\"name\":\"createPool\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"pool\",\"type\":\"address\"}],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint24\",\"name\":\"fee\",\"type\":\"uint24\"},{\"internalType\":\"int24\",\"name\":\"tickSpacing\",\"type\":\"int24\"}],\"name\":\"enableFeeAmount\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint24\",\"name\":\"\",\"type\":\"uint24\"}],\"name\":\"feeAmountTickSpacing\",\"outputs\":[{\"internalType\":\"int24\",\"name\":\"\",\"type\":\"int24\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"},{\"internalType\":\"uint24\",\"name\":\"\",\"type\":\"uint24\"}],\"name\":\"getPool\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"owner\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"parameters\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"factory\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"token0\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"token1\",\"type\":\"address\"},{\"internalType\":\"uint24\",\"name\":\"fee\",\"type\":\"uint24\"},{\"internalType\":\"int24\",\"name\":\"tickSpacing\",\"type\":\"int24\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"_owner\",\"type\":\"address\"}],\"name\":\"setOwner\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]",
+				FactoryAddress:       common.HexToAddress(ToChecksumAddress("0x1F98431c8aD98523631AE4a59f267346ea31F984")),
+				StartBlock:           big.NewInt(12369621),
+				PoolCreateMethodName: "PoolCreated",
+				Abi:                  "[{\"inputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"constructor\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"uint24\",\"name\":\"fee\",\"type\":\"uint24\"},{\"indexed\":true,\"internalType\":\"int24\",\"name\":\"tickSpacing\",\"type\":\"int24\"}],\"name\":\"FeeAmountEnabled\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"oldOwner\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"address\",\"name\":\"newOwner\",\"type\":\"address\"}],\"name\":\"OwnerChanged\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"token0\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"address\",\"name\":\"token1\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"uint24\",\"name\":\"fee\",\"type\":\"uint24\"},{\"indexed\":false,\"internalType\":\"int24\",\"name\":\"tickSpacing\",\"type\":\"int24\"},{\"indexed\":false,\"internalType\":\"address\",\"name\":\"pool\",\"type\":\"address\"}],\"name\":\"PoolCreated\",\"type\":\"event\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"tokenA\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"tokenB\",\"type\":\"address\"},{\"internalType\":\"uint24\",\"name\":\"fee\",\"type\":\"uint24\"}],\"name\":\"createPool\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"pool\",\"type\":\"address\"}],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint24\",\"name\":\"fee\",\"type\":\"uint24\"},{\"internalType\":\"int24\",\"name\":\"tickSpacing\",\"type\":\"int24\"}],\"name\":\"enableFeeAmount\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint24\",\"name\":\"\",\"type\":\"uint24\"}],\"name\":\"feeAmountTickSpacing\",\"outputs\":[{\"internalType\":\"int24\",\"name\":\"\",\"type\":\"int24\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"},{\"internalType\":\"uint24\",\"name\":\"\",\"type\":\"uint24\"}],\"name\":\"getPool\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"owner\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"parameters\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"factory\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"token0\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"token1\",\"type\":\"address\"},{\"internalType\":\"uint24\",\"name\":\"fee\",\"type\":\"uint24\"},{\"internalType\":\"int24\",\"name\":\"tickSpacing\",\"type\":\"int24\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"_owner\",\"type\":\"address\"}],\"name\":\"setOwner\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]",
 			},
 		},
 	}
+
 	return dexs
 }
 
 func scanLogFromRpc(dex Dex, blockSpace *big.Int) {
+
+	fmt.Println("我是go 程 ", dex.Name)
+
+	defer wg.Done()
 	client, err := ethclient.Dial(mainnetRpcWS)
 	if err != nil {
 		log.Fatal(err)
@@ -67,43 +130,49 @@ func scanLogFromRpc(dex Dex, blockSpace *big.Int) {
 	fmt.Println(currencyBlockNumber)
 	contractAddress := dex.Factory.FactoryAddress
 
-	query := ethereum.FilterQuery{
-		FromBlock: dex.Factory.StartBlock,
-		ToBlock:   big.NewInt(int64(currencyBlockNumber)),
-		Addresses: []common.Address{
-			contractAddress,
-		},
-	}
+	for startBlock := dex.Factory.StartBlock; startBlock.Cmp(big.NewInt(int64(currencyBlockNumber))) < 0; startBlock.Add(startBlock, blockSpace) {
 
-	logs, err := client.FilterLogs(context.Background(), query)
-	if err != nil {
-		log.Fatal(err)
-	}
+		fmt.Println("StartBlock", startBlock.String())
+		var endBlock *big.Int
+		if big.NewInt(0).Add(startBlock, blockSpace).Cmp(big.NewInt(int64(currencyBlockNumber))) > 0 {
+			endBlock = big.NewInt(int64(currencyBlockNumber))
+		} else {
+			endBlock = big.NewInt(0).Add(startBlock, blockSpace)
+		}
 
-	fmt.Println("logs", logs)
+		fmt.Println("EndBlock", endBlock.String())
 
-	contractAbi, err := abi.JSON(strings.NewReader(dex.Factory.Abi))
-	if err != nil {
-		log.Fatal(err)
-	}
+		query := ethereum.FilterQuery{
+			FromBlock: startBlock,
+			ToBlock:   endBlock,
+			Addresses: []common.Address{
+				contractAddress,
+			},
+		}
+		fmt.Println("当前扫描地址是", contractAddress)
 
-	for _, vLog := range logs {
-		fmt.Println(vLog.BlockHash.Hex()) // 0x3404b8c050aa0aacd0223e91b5c32fee6400f357764771d0684fa7b3f448f1a8
-		fmt.Println(vLog.BlockNumber)     // 2394201
-		fmt.Println(vLog.TxHash.Hex())    // 0x280201eda63c9ff6f305fcee51d5eb86167fab40ca3108ec784e8652a0e2b1a6
-
-		_, err := contractAbi.Unpack("PairCreated", vLog.Data)
+		logs, err := client.FilterLogs(context.Background(), query)
 		if err != nil {
 			log.Fatal(err)
 		}
+		fmt.Println(fmt.Sprintf("从 %v 到 %v 扫描到的日志数量 %v", startBlock.String(), endBlock.String(), len(logs)))
 
-		var topics [4]string
-		for i := range vLog.Topics {
-			topics[i] = vLog.Topics[i].Hex()
+		for _, vLog := range logs {
+
+			if len(vLog.Data) == 0 {
+				continue
+			}
+
+			if fn, ok := precessGetPairFromLogMap[dex.Name]; ok {
+				fn(vLog, dex)
+
+			} else {
+				fmt.Println("未找到处理函数")
+			}
+
 		}
-		fmt.Println(topics[0]) // 0xe79e73da417710ae99aa2088575580a60415d359acfad9cdd3382d59c80281d4
+		time.Sleep(1 * time.Millisecond)
 	}
-
 }
 
 func GetAllPools() {
@@ -112,10 +181,13 @@ func GetAllPools() {
 	dexs := initDexs()
 	// setp2: 遍历dex，获取所有的交易对
 	fmt.Println(dexs)
-	for _, dex := range dexs {
-		scanLogFromRpc(dex, big.NewInt(50000))
-	}
 
+	wg.Add(len(dexs))
+
+	for _, value := range dexs {
+		go scanLogFromRpc(value, big.NewInt(10000))
+	}
+	wg.Wait()
 }
 
 // tools Funcs
