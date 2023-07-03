ebd1cfafa6b9a97425e4cc67fdaac990f03cd92d
liurongdong <liurongdong@edaijia-inc.cn>
2023-07-03 16:05:03 +0800

    add cfmms

---
+++ Cffms_test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"hello/cfmms"
+	"testing"
+)
+
+func Test_Cfmms(t *testing.T) {
+	//cfmms.Cmd()
+	cfmms.GetAbiFromEtherscan("./cfmms/contracts_helper.json", "./cfmms/abi")
+	//cfmms.GenerateGoFilesByAbi("./cfmms/abi", "./cfmms/abi_go")
+}
--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 hello
 
 
+
+# 合约框架
+## foundry 文档
+https://learnblockchain.cn/docs/foundry/i18n/zh/index.html
+
+
 ## foundry init
 ```
  forge init --force --no-git
 ```
 
 
+## foundry test
+```
+ forge test -vvvvv  // v越多，输出的信息越详细 最多5个
+```
 
