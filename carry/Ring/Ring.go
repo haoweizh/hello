34cc3573bc81f5f41b6186833ea4fe56e34729f6
haoweizh <haoweizh@qq.com>
2023-09-01 18:02:56 +0800

    ring construct

--- carry/Ring/Ring.go
+++ carry/Ring/Ring.go
@@ -1,18 +1,149 @@
 package Ring
 
 import (
+	"fmt"
 	"hello/model"
 )
 
-var CompleteRings = make(map[string]*model.Ring)
+var CompleteRings = make(map[string]*Ring)
 
-func InitRingPool() {
+type Ring struct {
+	Complete           bool // already be a ring or not
+	LinkedCoins        map[string]bool
+	CoinHead, CoinTail string
+	SettingSide        map[string]string // settingKey - orderSide
+	SortedSettings     []string
+	Key                string
+}
 
+func (ring *Ring) Copy() (copy *Ring) {
+	copy = &Ring{
+		Complete:       ring.Complete,
+		LinkedCoins:    make(map[string]bool),
+		SettingSide:    make(map[string]string),
+		SortedSettings: make([]string, len(ring.SortedSettings)),
+		CoinHead:       ring.CoinHead,
+		CoinTail:       ring.CoinTail,
+		Key:            ring.Key}
+	for coin, b := range ring.LinkedCoins {
+		copy.LinkedCoins[coin] = b
+	}
+	for settingKey, side := range ring.SettingSide {
+		copy.SettingSide[settingKey] = side
+	}
+	for i, settingKey := range ring.SortedSettings {
+		copy.SortedSettings[i] = settingKey
+	}
+	return
+}
+
+func (ring *Ring) AddSetting(setting *model.Setting) (added bool) {
+	if ring.LinkedCoins == nil {
+		ring.LinkedCoins = make(map[string]bool)
+	}
+	if ring.SettingSide == nil {
+		ring.SettingSide = make(map[string]string)
+	}
+	if ring.SortedSettings == nil {
+		ring.SortedSettings = make([]string, 0)
+	}
+	if ring.Complete || ring.LinkedCoins[setting.Coin0] || ring.LinkedCoins[setting.Coin1] ||
+		(ring.CoinHead != setting.Coin0 && ring.CoinHead != setting.Coin1 && ring.CoinTail != setting.Coin0 && ring.CoinTail != setting.Coin1) {
+		return false
+	}
+	settingKey := setting.GetKey()
+	if ring.SettingSide[settingKey] != `` {
+		return false
+	}
+	if ring.CoinHead == setting.Coin0 {
+		ring.SettingSide[settingKey] = model.OrderSideBuy
+		ring.CoinHead = setting.Coin1
+		ring.LinkedCoins[setting.Coin0] = true
+		added = true
+	} else if ring.CoinHead == setting.Coin1 {
+		ring.SettingSide[settingKey] = model.OrderSideSell
+		ring.CoinHead = setting.Coin0
+		ring.LinkedCoins[setting.Coin1] = true
+		added = true
+	}
+	if ring.CoinTail == setting.Coin0 {
+		ring.SettingSide[settingKey] = model.OrderSideSell
+		ring.CoinTail = setting.Coin1
+		ring.LinkedCoins[setting.Coin0] = true
+		added = true
+	} else if ring.CoinTail == setting.Coin1 {
+		ring.SettingSide[settingKey] = model.OrderSideBuy
+		ring.CoinTail = setting.Coin0
+		ring.LinkedCoins[setting.Coin1] = true
+		added = true
+	}
+	if !added {
+		return added
+	}
+	if ring.LinkedCoins[setting.Coin0] && ring.LinkedCoins[setting.Coin1] {
+		ring.Complete = true
+	}
+	sortedSettings := make([]string, len(ring.SortedSettings)+1)
+	ring.Key = ``
+	index := 0
+	for ; index < len(ring.SortedSettings) && ring.SortedSettings[index] < settingKey; index++ {
+		sortedSettings[index] = ring.SortedSettings[index]
+	}
+	sortedSettings[index] = settingKey
+	for i := index + 1; i < len(sortedSettings); i++ {
+		sortedSettings[i] = ring.SortedSettings[i-1]
+	}
+	ring.SortedSettings = sortedSettings
+	for i, sortedSetting := range sortedSettings {
+		if i == 0 {
+			ring.Key = fmt.Sprintf(`%s_%v`, sortedSetting, ring.SettingSide[sortedSetting])
+		} else {
+			ring.Key = fmt.Sprintf(`%s_%s_%v`, ring.Key, sortedSetting, ring.SettingSide[sortedSetting])
+		}
+	}
+	return added
+}
+
+func (ring *Ring) Equals(compRing *Ring) (isEqual bool) {
+	if compRing == nil || ring.SettingSide == nil || compRing.SettingSide == nil || len(ring.SettingSide) != len(compRing.SettingSide) {
+		return false
+	}
+	for settingKey, orderSide := range ring.SettingSide {
+		if compRing.SettingSide[settingKey] != orderSide {
+			return false
+		}
+	}
+	return true
+}
+
+func InitRingPool(settings []*model.Setting) {
+	CompleteRings = make(map[string]*Ring)
+	rings := make(map[string]*Ring)
+	for _, setting := range settings {
+		settingKey := setting.GetKey()
+		ringKey := fmt.Sprintf(`%s_%s`, settingKey, model.OrderSideSell)
+		rings[ringKey] = &Ring{
+			LinkedCoins:    make(map[string]bool),
+			CoinHead:       setting.Coin0,
+			CoinTail:       setting.Coin1,
+			SettingSide:    map[string]string{settingKey: model.OrderSideSell},
+			SortedSettings: []string{settingKey},
+			Key:            ringKey}
+		ringKey = fmt.Sprintf(`%s_%s`, settingKey, model.OrderSideBuy)
+		rings[ringKey] = &Ring{
+			LinkedCoins:    make(map[string]bool),
+			CoinHead:       setting.Coin1,
+			CoinTail:       setting.Coin0,
+			SettingSide:    map[string]string{settingKey: model.OrderSideBuy},
+			SortedSettings: []string{settingKey},
+			Key:            ringKey}
+	}
+	addStep(rings, settings)
 }
 
 // EnhanceRingPool 假定当前是n阶，增强为n+1阶
-func addStep(stepRings map[string]*model.Ring, settings []*model.Setting) (nextRings map[string]*model.Ring) {
-	nextRings = make(map[string]*model.Ring)
+func addStep(stepRings map[string]*Ring, settings []*model.Setting) (nextRings map[string]*Ring) {
+	nextRings = make(map[string]*Ring)
 	for _, ring := range stepRings {
 		for _, setting := range settings {
 			copyRing := ring.Copy()
@@ -26,6 +157,7 @@ func addStep(stepRings map[string]*model.Ring, settings []*model.Setting) (nextR
 		}
 	}
 	if len(nextRings) > 0 {
+		fmt.Println(fmt.Sprintf(`level %d`, len(nextRings)))
 		addStep(nextRings, settings)
 	}
 	return nextRings
---
+++ carry/Ring/Ring_test.go
@@ -0,0 +1,35 @@
+package Ring
+
+import (
+	"fmt"
+	"hello/model"
+	"testing"
+)
+
+func TestRing(t *testing.T) {
+	settings := make([]*model.Setting, 0)
+	markets := []string{`甲`, `乙`, `丙`, `丁`}
+	coin0s := []string{`btc`, `eth`, `a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`, `i`}
+	coin1s := []string{`btc`, `eth`}
+	for _, market := range markets {
+		for _, coin0 := range coin0s {
+			for _, coin1 := range coin1s {
+				if coin0 >= coin1 {
+					continue
+				}
+				settings = append(settings, &model.Setting{
+					Function: "ring",
+					Market:   market,
+					Symbol:   fmt.Sprintf(`%s_%s`, coin0, coin1),
+					Way:      "spot",
+					Coin0:    coin0,
+					Coin1:    coin1,
+				})
+			}
+		}
+	}
+	InitRingPool(settings)
+	for s := range CompleteRings {
+		fmt.Println(s)
+	}
+}
