325dfe65ee5c0dd4938d71d9e0ca5df71bc99b3d
haoweizh <haoweizh@qq.com>
2023-10-08 12:34:15 +0800

    add function grid

--- carry/queue/Queue.go
+++ carry/queue/Queue.go
@@ -69,9 +69,9 @@ var ProcessQueue = func(setting *model.Setting, tick *model.BidAsk) {
 					model.OrderTypeLimit, order.OrderId)
 				if success {
 					delete(data.QueueOrders, order.OrderId)
-					util.Notice(fmt.Sprintf(`clear all order when can not queue code %d %s %s %s left %d tick [%e %e %e %e]`,
-						queueCode, setting.Market, setting.Symbol, order.OrderId, len(data.QueueOrders), tick.Bids[0].Price,
-						tick.Bids[0].Amount, tick.Asks[0].Price, tick.Asks[0].Amount))
+					util.Notice(fmt.Sprintf(`clear all order when can not queue code %d %s %s %s left %d tick [%e %e %e %e] liq[%e %e %e %e]`,
+						queueCode, setting.Market, setting.Symbol, order.OrderId, len(data.QueueOrders), tick.Bids[0].Price, tick.Bids[0].Amount,
+						tick.Asks[0].Price, tick.Asks[0].Amount, tickLiq.Bids[0].Price, tickLiq.Bids[0].Amount, tickLiq.Asks[0].Price, tickLiq.Asks[0].Amount))
 					time.Sleep(time.Second)
 				}
 			}
