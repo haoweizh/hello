3966bb2f4b34f54f95fa1b08c4771f68ae8aff54
张浩伟 <zhanghaowei@edaijia-inc.cn>
2023-12-02 23:14:33 +0800

    add gate log

--- api/Gate.go
+++ api/Gate.go
@@ -510,7 +510,7 @@ var subscribeHandler = func(market string, connection *websocket.Conn, subscribe
 			if err = SendToConnection(model.Gate, connection, subscribeMessage); err != nil {
 				util.SocketInfo(" gate can not subscribe spot order book symbol %s %s", subscribeMessage, err.Error())
 			} else {
-				util.Notice(`gate sub order_book %s`, string(subscribeMessage))
+				util.Notice(`gate sub order_book %s symbols %v to %s`, string(subscribeMessage), subscribe, dialectSymbol)
 			}
 			time.Sleep(10 * time.Millisecond)
 		}
