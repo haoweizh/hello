1c44ebfbcdb78be2a883a3e1ea9b3d25930991a0
haoweizh <haoweizh@qq.com>
2024-01-05 10:12:57 +0800

    如果本周期海龟有平仓，不再新开海龟

--- carry/Turtle/CombineTurtle.go
+++ carry/Turtle/CombineTurtle.go
@@ -252,7 +252,7 @@ func handleBreak(setting *model.Setting, data *model.TurtleData, orders []*model
 	}
 	util.Notice(fmt.Sprintf(`query %s break %s %s %s %d %s %s chances %d`,
 		setting.Function, setting.Market, setting.Symbol, orders[0].OrderSide, len(orders), orders[0].OrderId, orders[0].Function, setting.Chance))
-	if orders[0].Function == model.Close || orders[0].OrderType == model.OrderTypeTrailStop {
+	if (orders[0].RefreshType != model.FunctionTurtleAdjust && orders[0].Function == model.Close) || orders[0].OrderType == model.OrderTypeTrailStop {
 		msg := fmt.Sprintf(`liquidate: %s %s chance:%d Amount:%e px:%e`,
 			setting.Market, setting.Symbol, setting.Chance, setting.GridAmount, setting.PriceX)
 		go api.SendMails(`平`+setting.Market+setting.Symbol, msg)
