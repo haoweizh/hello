81549383685fc5ec26b36ca1adf8a5ab26f8d0d8
张浩伟 <zhanghaowei@edaijia-inc.cn>
2024-02-02 17:28:08 +0800

    update 4h parameter

--- api/TurtleCommon.go
+++ api/TurtleCommon.go
@@ -479,7 +479,7 @@ func getTurtleCandles(account *model.Account, market, symbol string, far, second
 	calcLenN := 10
 	calcLenV := 5
 	if seconds == 14400 {
-		calcLenN = 20
+		calcLenN = 15
 		calcLenV = 10
 	}
 	sortedCandles := &model.SortedCandle{Value: candles}
