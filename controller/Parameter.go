84aa274839e863b6847fbfd1689d6e4c17738830
张浩伟 <zhanghaowei@edaijia-inc.cn>
2024-04-08 23:51:32 +0800

    update lowRate

--- controller/Parameter.go
+++ controller/Parameter.go
@@ -232,6 +232,8 @@ func simulateGrid(c *gin.Context) {
 			util.StoreSyncMap(&model.CarryInfo, fmt.Sprintf(`get market candle %s %s from %s len %d`,
 				market, symbol, begin.String(), len(candles)), `gridInfo`)
 			settings := []*model.Setting{
+				{Market: market, Symbol: symbol, Coin: coin, Function: fmt.Sprintf(`1.3_20_86400_%s_%s`, strBegin, strEnd), Far: 20, Near: 10, Seconds: 86400, CloseShortMargin: 1.3},
+				{Market: market, Symbol: symbol, Coin: coin, Function: fmt.Sprintf(`1.3_60_14400_%s_%s`, strBegin, strEnd), Far: 60, Near: 30, Seconds: 14400, CloseShortMargin: 1.3},
 				{Market: market, Symbol: symbol, Coin: coin, Function: fmt.Sprintf(`1.4_20_86400_%s_%s`, strBegin, strEnd), Far: 20, Near: 10, Seconds: 86400, CloseShortMargin: 1.4},
 				{Market: market, Symbol: symbol, Coin: coin, Function: fmt.Sprintf(`1.4_60_14400_%s_%s`, strBegin, strEnd), Far: 60, Near: 30, Seconds: 14400, CloseShortMargin: 1.4},
 				{Market: market, Symbol: symbol, Coin: coin, Function: fmt.Sprintf(`1.45_20_86400_%s_%s`, strBegin, strEnd), Far: 20, Near: 10, Seconds: 86400, CloseShortMargin: 1.45},
