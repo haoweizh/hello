22108ea39fae3c0676fd23f6a32963865c22b2de
张浩伟 <zhanghaowei@edaijia-inc.cn>
2024-04-22 12:01:08 +0800

    add kline ws

--- api/BitgetSpot.go
+++ api/BitgetSpot.go
@@ -90,7 +90,6 @@ func WsDepthServeBitgetSpot(markets *model.Markets) (channels []chan struct{}, e
 				return
 			}
 			if markets.SetBidAsk(symbol, model.BitgetSpot, &bidAsk) {
-				util.Info(fmt.Sprintf("success get bitget ticker: %s %f", symbol, bidAsk.Asks[0].Price))
 				funcHandlers := GetFunctions(model.BitgetSpot, symbol)
 				if funcHandlers != nil {
 					funcHandlers.Range(func(function, value interface{}) bool {
