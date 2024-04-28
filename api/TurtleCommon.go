074ab365e7f485c8dad875f4b15cd3b8b37974ab
张浩伟 <zhanghaowei@edaijia-inc.cn>
2024-04-28 21:19:57 +0800

    rename market connections

--- api/TurtleCommon.go
+++ api/TurtleCommon.go
@@ -733,9 +733,9 @@ func CanOpenCombine(settingCombine, settingNormal *model.Setting, dataTurtle *mo
 		}
 	} else {
 		if settingCombine.Seconds == 14400 {
-			lowRate = 1.15
+			lowRate = 1.2
 		} else {
-			lowRate = 1.3
+			lowRate = 1.35
 		}
 		settingsNormal.Range(sumTurtle)
 		settingsCombine.Range(sumCombineOnly)
