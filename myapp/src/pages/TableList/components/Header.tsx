66b17c38d9a46cfe34e056853984f84c8b30cd01
east <57059329@qq.com>
2024-04-30 10:03:34 +0800

    feat: ping message

--- myapp/src/pages/TableList/components/Header.tsx
+++ myapp/src/pages/TableList/components/Header.tsx
@@ -35,39 +35,39 @@ const Header:React.FC<iHeader> = (props)=>{
 
   return (
     <Flex justify={'space-between'} >
-      <Flex justify={"space-around"}  style={{fontSize: "11px", maxWidth:"400px"}} gap={10}>
+      <Flex justify={"space-around"}  style={{fontSize: "15px", maxWidth:"400px"}} gap={10}>
        <div>
          <Flex >
-           <Text style={{fontSize:"11px"}}>Symbol:</Text>
-           <Text  style={{fontSize:"11px"}} type={'danger'}>{item.Symbol}</Text>
+           <Text style={{fontSize:"15px"}}>Symbol:</Text>
+           <Text  style={{fontSize:"15px"}} type={'danger'}>{item.Symbol}</Text>
          </Flex>
          <Flex >
-           <Text  style={{fontSize:"11px"}} >Market:</Text>
-           <Text  style={{fontSize:"11px"}} type={'danger'}>{item.Market}</Text>
+           <Text  style={{fontSize:"15px"}} >Market:</Text>
+           <Text  style={{fontSize:"15px"}} type={'danger'}>{item.Market}</Text>
          </Flex>
          <Flex >
-           <Text  style={{fontSize:"11px"}}>IntervalSeconds:</Text>
-           <Text  style={{fontSize:"11px"}} type={'danger'}>{item.IntervalSeconds}</Text>
+           <Text  style={{fontSize:"15px"}}>IntervalSeconds:</Text>
+           <Text  style={{fontSize:"15px"}} type={'danger'}>{item.IntervalSeconds}</Text>
          </Flex>
          <Flex>
-           <Text  style={{fontSize:"11px"}}>WarnChange:</Text>
-           <Text  style={{fontSize:"11px"}} type={'danger'}>{item.WarnChange}</Text>
+           <Text  style={{fontSize:"15px"}}>WarnChange:</Text>
+           <Text  style={{fontSize:"15px"}} type={'danger'}>{item.WarnChange}</Text>
          </Flex>
        </div>
 
         <div>
 
           <Flex >
-            <Text  style={{fontSize:"11px"}}>WarnIncrease:</Text>
-            <Text   style={{fontSize:"11px"}} type={'danger'}>{item.WarnIncrease}</Text>
+            <Text  style={{fontSize:"15px"}}>WarnIncrease:</Text>
+            <Text   style={{fontSize:"15px"}} type={'danger'}>{item.WarnIncrease}</Text>
           </Flex>
           <Flex >
-            <Text  style={{fontSize:"11px"}} >WarnVolume:</Text>
-            <Text  style={{fontSize:"11px"}} type={'danger'}>{item.WarnVolume}</Text>
+            <Text  style={{fontSize:"15px"}} >WarnVolume:</Text>
+            <Text  style={{fontSize:"15px"}} type={'danger'}>{item.WarnVolume}</Text>
           </Flex>
           <Flex >
-            <Text  style={{fontSize:"11px"}}>CreateTime:</Text>
-            <Text  style={{fontSize:"11px"}} type={'danger'}>{moment(item.CreatedAt).format("YYYY-MM-DD HH:mm:ss")}</Text>
+            <Text  style={{fontSize:"15px"}}>CreateTime:</Text>
+            <Text  style={{fontSize:"15px"}} type={'danger'}>{moment(item.CreatedAt).format("YYYY-MM-DD HH:mm:ss")}</Text>
           </Flex>
         </div>
 
