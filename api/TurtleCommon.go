f33783cb7b6a404b0724dc7a3c31a0ad2a367e9f
haoweizh <haoweizh@qq.com>
2024-05-06 17:40:46 +0800

    turtle parameter update

--- api/TurtleCommon.go
+++ api/TurtleCommon.go
@@ -729,7 +729,7 @@ func CanOpenCombine(settingCombine, settingNormal *model.Setting, dataTurtle *mo
 		if settingCombine.Seconds == 14400 {
 			lowRate = 1.1
 		} else {
-			lowRate = 1.2
+			lowRate = 1.18
 		}
 	} else {
 		if settingCombine.Seconds == 14400 {
