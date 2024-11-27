6561630721804623461de569ace2c8374884d661
haoweizh <haoweizh@qq.com>
2024-11-27 08:54:46 +0800

    fix binance dis connect issue

--- api/FuncWS.go
+++ api/FuncWS.go
@@ -125,7 +125,8 @@ func CreateWSTick(environment *model.Environment, market string) (
 			subscribeHandlerBinance, wsHandlerBinance, wsStepBinance)
 	case model.BinancePerp:
 		socketMap, channels, err = WebSocketClient(market, wsBinancePerp+`/stream`, GetWSSubscribes(
-			market, []string{model.SubscribeTicker, model.SubscribeMarkPrice}), subscribeHandlerBinance, wsHandlerBinancePerp, wsStepBinance)
+			market, []string{model.SubscribeTicker}), subscribeHandlerBinance, wsHandlerBinancePerp, wsStepBinance)
+		// , model.SubscribeMarkPrice
 	case model.HuobiPerp:
 		socketMap, channels, err = WebSocketClient(market, wsHuobiPerp, GetWSSubscribes(model.HuobiPerp, []string{model.SubscribeDepth}),
 			subscribeHandlerHuobiPerp, wsMsgHandler, wsStepHuobi)
