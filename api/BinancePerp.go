82ba27ac134b80ab337e7dc500ee126ec059ce83
haoweizh <haoweizh@qq.com>
2024-11-27 09:02:19 +0800

    fix binance dis connect issue

--- api/BinancePerp.go
+++ api/BinancePerp.go
@@ -157,6 +157,7 @@ var wsHandlerBinancePerp = func(market string, event []byte) {
 	id := result.Get("id").MustInt()
 	if id > 0 {
 		subIdBinance.Store(id, false)
+		util.Notice(`%s success sub id %s %d`, market, id)
 	}
 	subscribe, _ := result.Get("stream").String()
 	result = result.Get(`data`)
