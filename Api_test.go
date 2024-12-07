a3619d1848f1e8257222d7ef359846cb4aa08b4e
haoweizh <haoweizh@qq.com>
2024-12-08 01:32:00 +0800

    add gate apis

--- Api_test.go
+++ Api_test.go
@@ -764,32 +764,32 @@ func Test_GateSols(t *testing.T) {
 	fmt.Println(coins)
 }
 
-func Test_C(t *testing.T) {
-	// Initialize market publisher
-	marketPublisher, err := util.NewMarketPublisher("market_topic")
-	if err != nil {
-
-		log.Fatalf("Failed to create market publisher: %v", err)
-	}
-	defer marketPublisher.Close()
-
-	// Publish a market message
-	err = marketPublisher.MarketPublish("Hello, Market!")
-	if err != nil {
-		log.Fatalf("Failed to publish market message: %v", err)
-	}
-
-	// Initialize market receiver
-	marketReceiver, err := util.NewMarketReceiver("market_topic")
-	if err != nil {
-		log.Fatalf("Failed to create market receiver: %v", err)
-	}
-	defer marketReceiver.Close()
-
-	// Receive a market message
-	message := marketReceiver.MarketReceive(1024)
-	if err != nil {
-		log.Fatalf("Failed to receive market message: %v", err)
-	}
-	fmt.Println("Received market message:", message)
-}
+//func Test_C(t *testing.T) {
+//	// Initialize market publisher
+//	marketPublisher, err := util.NewMarketPublisher("market_topic")
+//	if err != nil {
+//
+//		log.Fatalf("Failed to create market publisher: %v", err)
+//	}
+//	defer marketPublisher.Close()
+//
+//	// Publish a market message
+//	err = marketPublisher.MarketPublish("Hello, Market!")
+//	if err != nil {
+//		log.Fatalf("Failed to publish market message: %v", err)
+//	}
+//
+//	// Initialize market receiver
+//	marketReceiver, err := util.NewMarketReceiver("market_topic")
+//	if err != nil {
+//		log.Fatalf("Failed to create market receiver: %v", err)
+//	}
+//	defer marketReceiver.Close()
+//
+//	// Receive a market message
+//	message := marketReceiver.MarketReceive(1024)
+//	if err != nil {
+//		log.Fatalf("Failed to receive market message: %v", err)
+//	}
+//	fmt.Println("Received market message:", message)
+//}
