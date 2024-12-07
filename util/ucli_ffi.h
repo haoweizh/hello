b22debe3052c96fad78095fef29670b27b31114a
shenzh1990 <7929255+shenzh1990@users.noreply.github.com>
2024-12-08 00:13:10 +0800

    提交C++ 语言对应的函数调用类以及调用方式，暂时无法调试。

--- util/ucli_ffi.h
+++ util/ucli_ffi.h
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #ifndef UCLI_FFI_H
 #define UCLI_FFI_H
 struct UltraMarketPublisher {
@@ -22,14 +23,30 @@ struct UltraOrderReceiver {
 
 
 
-void init_market_publisher(char* topic, struct UltraMarketPublisher* publisher);
-void init_order_publisher(char* topic, struct UltraOrderPublisher* publisher);
-void init_market_receiver(char* topic, struct UltraMarketReceiver* receiver);
-void init_order_receiver(char* topic, struct UltraOrderReceiver* receiver);
+void init_market_publisher(char* topic, struct UltraMarketPublisher* publisher){
+ printf("Hello from C!\n");
+ }
+void init_order_publisher(char* topic, struct UltraOrderPublisher* publisher){
+                                                                              printf("Hello from C!\n");
+                                                                              }
+void init_market_receiver(char* topic, struct UltraMarketReceiver* receiver){
+                                                                             printf("Hello from C!\n");
+                                                                             }
+void init_order_receiver(char* topic, struct UltraOrderReceiver* receiver){
+                                                                           printf("Hello from C!\n");
+                                                                           }
 
-void publish_market(struct UltraMarketPublisher* publisher, char* msg);
-void publish_order(struct UltraOrderPublisher* publisher, char* msg);
-void receive_market(struct UltraMarketReceiver* receiver, char* buf);
-void receive_order(struct UltraOrderReceiver* receiver, char* buf);
+void publish_market(struct UltraMarketPublisher* publisher, char* msg){
+                                                                       printf("Hello from C!\n");
+                                                                       }
+void publish_order(struct UltraOrderPublisher* publisher, char* msg){
+                                                                     printf("Hello from C!\n");
+                                                                     }
+void receive_market(struct UltraMarketReceiver* receiver, char* buf){
+                                                                     printf("Hello from C!\n");
+                                                                     }
+void receive_order(struct UltraOrderReceiver* receiver, char* buf){
+                                                                   printf("Hello from C!\n");
+                                                                   }
 
 #endif //UCLI_FFI_H
