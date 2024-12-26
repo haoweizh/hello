78fb854cb6ded3746df0b881847227d1d5b0b8a8
haoweizh <haoweizh@qq.com>
2024-12-26 21:41:19 +0800

    bitget合约持仓个数超过限额后，只让那些没有持仓的币种停止开仓

--- carry/cross/Cross.go
+++ carry/cross/Cross.go
@@ -165,7 +165,7 @@ func createFromPosition(account *model.Account, setting *model.Setting, valueLim
 	}
 	if cm.contractValueInU/cm.accountValueInU > rateLimitPosition || valueInUsd > valueLimit ||
 		valueInUsd/cm.accountValueInU > rateLimitHolding ||
-		(setting.Market == model.BitgetPerp && (len(cm.positions) > BitgetPosLimit) && carryStatus.Holding != 0) {
+		(setting.Market == model.BitgetPerp && (len(cm.positions) > BitgetPosLimit && carryStatus.Holding == 0)) {
 		util.Log(util.LogLevelError, fmt.Sprintf(`do revert true %s %s value big %f %f %f %f %f %f pos len %d`,
 			setting.Market, setting.Symbol, cm.contractValueInU, cm.accountValueInU, rateLimitPosition, valueInUsd, valueLimit, rateLimitHolding, len(cm.positions)))
 		doRevert = true
