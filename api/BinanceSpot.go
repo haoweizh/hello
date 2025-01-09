1dcd07fc3c51578221c54e1e2e4d97f2e87cde32
shenzh1990 <7929255+shenzh1990@users.noreply.github.com>
2025-01-09 10:11:13 +0800

    1.新增binance关于ACCOUNT_UPDATE的通道 2.实现collateral收集

--- api/BinanceSpot.go
+++ api/BinanceSpot.go
@@ -386,6 +386,7 @@ func parseOrderBinanceSpot(market string, orderJson *simplejson.Json) (order *mo
 }
 
 var wsOrderUpdateBinance = func(market, key string, msg []byte) {
+	util.Log(util.LogLevelInfo, fmt.Sprintf(" %s msg: %s", market, string(msg)))
 	resJson, _ := util.NewJSON(msg)
 	if resJson == nil {
 		return
@@ -401,6 +402,18 @@ var wsOrderUpdateBinance = func(market, key string, msg []byte) {
 		dealAmount, _ := strconv.ParseFloat(resJson.Get(`z`).MustString(), 64)
 		status := model.GetOrderStatus(market, resJson.Get(`X`).MustString())
 		UpdateOrderDeal(market, orderId, status, string(msg), dealAmount)
+	case `ACCOUNT_UPDATE`:
+		collateral := &model.Collateral{AccountKey: key}
+		dataarray := resJson.GetPath(`a`, `B`).MustArray()
+		for _, v := range dataarray {
+			value := v.(map[string]interface{})
+			if value[`a`] != nil && value[`a`] == `USDT` {
+				collateral.Available, _ = strconv.ParseFloat(value[`cw`].(string), 64)
+			}
+		}
+		util.Log(util.LogLevelInfo, fmt.Sprintf("binance unified %s %f", collateral.AccountKey, collateral.Available))
+		model.CollateralHandler(collateral)
+
 	}
 }
 
