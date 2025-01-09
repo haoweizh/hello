1bc62cfbf3caa67061c53d501ec2e86496b32343
haoweizh <haoweizh@qq.com>
2025-01-09 12:23:10 +0800

    采用cron 控制clear cross

--- carry/DBHandler.go
+++ carry/DBHandler.go
@@ -181,8 +181,9 @@ func Maintain() {
 	}
 	if initCross && model.AppConfig.Handle == `1` {
 		go cross.ContinueComp()
+		go cross.ClearCross()
 		c := cron.New()
-		_, err := c.AddFunc(`0 0,30 * * * ?`, cross.ClearCross)
+		_, err := c.AddFunc(`0 0,30 * * *`, cross.ClearCross)
 		if err != nil {
 			util.Log(util.LogLevelError, `fail to cron clear cross `+err.Error())
 		} else {
