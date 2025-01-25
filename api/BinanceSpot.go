ee66ad7da0aa545cec3d0f8728065d8b126a857b
haoweizh <haoweizh@qq.com>
2025-01-25 18:21:05 +0800

    更新账户余额是区分现货期货统一账户

--- api/BinanceSpot.go
+++ api/BinanceSpot.go
@@ -413,8 +413,11 @@ var wsActHandlerBinance = func(market, key string, event []byte) {
 			if status == 200 {
 				collateral := &model.Collateral{AccountKey: key}
 				collateral.Available, _ = strconv.ParseFloat(responseJson.GetPath(`result`, `totalMarginBalance`).MustString(), 64)
-				collateral.AccountValueInU, _ = strconv.ParseFloat(responseJson.GetPath(`result`, `totalCrossWalletBalance`).MustString(), 64)
-				//util.Log(util.LogLevelInfo, fmt.Sprintf("binance unified %s %f", collateral.AccountKey, collateral.Available))
+				collateral.AccountValueInU, _ = strconv.ParseFloat(responseJson.GetPath(`result`, `totalWalletBalance`).MustString(), 64)
+				totalUnrealizedProfit, _ := strconv.ParseFloat(responseJson.GetPath(`result`, `totalUnrealizedProfit`).MustString(), 64)
+				collateral.AccountValueInU += totalUnrealizedProfit
+				util.Log(util.LogLevelInfo, fmt.Sprintf("binance unified %s %f %f %s %f", collateral.AccountKey,
+					collateral.Available, collateral.AccountValueInU, responseJson.GetPath(`result`, `totalWalletBalance`), totalUnrealizedProfit))
 				model.CollateralHandler(key, model.MarketTypePerp, false, collateral)
 			} else {
 				code := responseJson.GetPath(`error`, `code`).MustInt()
