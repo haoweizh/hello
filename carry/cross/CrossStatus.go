14d76e4923939c9e0290c3b652be419446a7699b
haoweizh <haoweizh@qq.com>
2025-02-05 20:56:03 +0800

    资金费率处理2/T+(1-t/T)^2

--- carry/cross/CrossStatus.go
+++ carry/cross/CrossStatus.go
@@ -69,7 +69,7 @@ type spotMarket struct {
 	reduceOnly      bool // 只减仓模式
 }
 
-// 1/T+(1-t/T)^2
+// 2/T+(1-t/T)^2
 func handledFRate(account *model.Account, market, symbol string, interval int) (got, delayed bool, fundingRate *model.FundingRate, handledFr float64) {
 	got, delayed, fundingRate = api.GetFundingRate(account, market, symbol, false)
 	if !got {
@@ -84,7 +84,7 @@ func handledFRate(account *model.Account, market, symbol string, interval int) (
 	if leftHours > hours {
 		leftHours = hours
 	}
-	handledFr = fundingRate.Rate * (1/hours + (1-leftHours/hours)*(1-leftHours/hours))
+	handledFr = fundingRate.Rate * (2/hours + (1-leftHours/hours)*(1-leftHours/hours))
 	if handledFr > 0.1 || handledFr < -0.1 {
 		got = false
 		util.Log(util.LogLevelError, fmt.Sprintf(`fatal error funding rate break %s %s %f %#v %d`,
