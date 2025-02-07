b5ba5a71493694d3f658178e896e9226f4248554
haoweizh <haoweizh@qq.com>
2025-02-07 18:02:41 +0800

    增加今日资金费率金额

--- model/Market.go
+++ model/Market.go
@@ -72,7 +72,7 @@ type Environment struct {
 	PubChanNeedReset                  sync.Map // market - bool
 	PubSubscribes                     sync.Map // market*wsUrl - []interface{}
 	OkexPubMarkets                    sync.Map // channel*instid - string
-	FundingFeeToday                   sync.Map // accountKey*market*symbol - value in usdt
+	FundingFeeToday                   sync.Map // account index*market*symbol - value in usdt
 }
 
 type MarkPriceInfo struct {
