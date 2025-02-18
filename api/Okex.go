e6826239e83221faf51cf4e8499b775ce3399868
haoweizh <haoweizh@qq.com>
2025-02-18 20:27:57 +0800

    处理deListing状态

--- api/Okex.go
+++ api/Okex.go
@@ -297,6 +297,9 @@ var wsAccountHandlerOKEX = func(market, key string, event []byte) {
 				for _, item := range data[`balData`].([]interface{}) {
 					var balance *model.Balance
 					value := item.(map[string]interface{})
+					if value[`ccy`] == nil || value[`uTime`] == nil || value[`cashBal`] == nil {
+						continue
+					}
 					balance.Coin = value[`ccy`].(string)
 					balance.Amount, _ = strconv.ParseFloat(value[`cashBal`].(string), 64)
 					ts, _ := strconv.ParseInt(value[`uTime`].(string), 10, 64)
