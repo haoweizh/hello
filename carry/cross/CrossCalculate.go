095c1096e7e3825c1e6f32de5213c5a7975008e0
haoweizh <haoweizh@qq.com>
2025-03-07 22:18:32 +0800

    add debug log for grid cross

--- carry/cross/CrossCalculate.go
+++ carry/cross/CrossCalculate.go
@@ -414,6 +414,7 @@ func calcAmount(index int, coin string, carryStatus, carryStatusRelate *model.Ca
 	scoreR = scoreOpenR
 	scoreSwitchR = scoreOpenR
 	scoreCloseR = scoreOpenR
+	doLog := false
 	logMsg := fmt.Sprintf(`score check1 %s %s %f %f rate %f score %f %s %s %f %f rate %f score %f`,
 		carryStatus.Market, carryStatus.Symbol, priceBid, priceAsk, handledRate, score, carryStatusRelate.Market, carryStatusRelate.Symbol, priceBidRelate, priceAskRelate, handledRateRelate, scoreR)
 	if handledRateRelate > handledRate { // R为买方＞0
@@ -445,8 +446,11 @@ func calcAmount(index int, coin string, carryStatus, carryStatusRelate *model.Ca
 		priceBuy = tickRelate.Asks[0].Price
 		askAmount = tick.Bids[0].Amount
 		bidAmount = tickRelate.Asks[0].Amount
+		if statusBuy.TradeLineBuy == 0.001 {
+			doLog = true
+		}
 		logMsg += fmt.Sprintf(`score %s %s at %f %f use score %f %s line %f`,
-			statusBuy.Market, statusBuy.Symbol, priceBuy, priceSell, scoreUseR, scoreTypeR, statusBuy.TradeLineBuy)
+			statusBuy.Market, statusBuy.Symbol, priceBuy, priceSell, scoreUse, scoreType, statusBuy.TradeLineBuy)
 		_, _, scoreUseR, scoreTypeR = checkTradeLine(carryStatus, carryStatusRelate, carryCoin, tick.Asks[0].Price, tickRelate.Bids[0].Price, scoreOpenR, scoreCloseR, scoreSwitchR)
 	} else {
 		valid, amountLimit, scoreUseR, scoreTypeR = checkTradeLine(carryStatus, carryStatusRelate, carryCoin, tick.Asks[0].Price, tickRelate.Bids[0].Price, scoreOpenR, scoreCloseR, scoreSwitchR)
@@ -457,6 +461,9 @@ func calcAmount(index int, coin string, carryStatus, carryStatusRelate *model.Ca
 			priceBuy = tick.Asks[0].Price
 			askAmount = tickRelate.Bids[0].Amount
 			bidAmount = tick.Asks[0].Amount
+			if statusBuy.TradeLineBuy == 0.001 {
+				doLog = true
+			}
 			logMsg += fmt.Sprintf(`score %s %s at %f %f use score %f %s line %f`,
 				statusBuy.Market, statusBuy.Symbol, priceBuy, priceSell, scoreUseR, scoreTypeR, statusBuy.TradeLineBuy)
 		}
@@ -483,7 +490,7 @@ func calcAmount(index int, coin string, carryStatus, carryStatusRelate *model.Ca
 			return false, nil, nil, 0, 0, 0
 		}
 	}
-	if amount > 0 && index == 0 {
+	if amount > 0 && index == 0 && doLog {
 		util.Log(util.LogLevelInfo, fmt.Sprintf(`%s amt %f`, logMsg, amount))
 	}
 	return false, statusBuy, statusSell, amount, priceBuy, priceSell
