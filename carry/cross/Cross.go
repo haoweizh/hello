4f2d608abbbc04ab3caf010807d478543c3d0de8
haoweizh <haoweizh@qq.com>
2025-03-15 08:55:28 +0800

    持仓查询还是改到15，45。compall改到15

--- carry/cross/Cross.go
+++ carry/cross/Cross.go
@@ -418,7 +418,7 @@ var ClearCross = func() {
 	}
 	model.AppEnvironment.CrossEqualing = true
 	doEqual := false
-	if time.Now().Minute() == 35 {
+	if time.Now().Minute() == 15 {
 		doEqual = true
 	} else {
 		model.AppEnvironment.FundingFeeToday.Clear()
