c7d4bc5ef8c11f576c8424c96921cad57b8b7021
haoweizh <haoweizh@qq.com>
2025-03-20 10:14:10 +0800

    所有平仓交易都不限制

--- carry/cross/CrossCalculate.go
+++ carry/cross/CrossCalculate.go
@@ -135,8 +135,17 @@ var ProcessCrossPositions = func(market, accountKey string, positions []*model.P
 				if account == nil {
 					continue
 				}
-				util.StoreSyncMap(&model.AppEnvironment.PauseTrade, true, setting.Coin, setting.Market, setting.Symbol, account.Key, model.OrderSideSell)
-				util.StoreSyncMap(&model.AppEnvironment.PauseTrade, true, setting.Coin, setting.Market, setting.Symbol, account.Key, model.OrderSideBuy)
+				item, _ := util.LoadSyncMap(carryStatusMap, setting.Coin, setting.Market, setting.Symbol, account.Key)
+				if item == nil {
+					continue
+				}
+				status := item.(*model.CarryStatus)
+				if status.Holding >= 0 {
+					util.StoreSyncMap(&model.AppEnvironment.PauseTrade, true, setting.Coin, setting.Market, setting.Symbol, account.Key, model.OrderSideBuy)
+				}
+				if status.LimitSell >= 0 {
+					util.StoreSyncMap(&model.AppEnvironment.PauseTrade, true, setting.Coin, setting.Market, setting.Symbol, account.Key, model.OrderSideSell)
+				}
 				util.Log(util.LogLevelError, fmt.Sprintf(`pause trade when update position %s %d %s %f setting %s %s holding %e value %e`,
 					market, account.Index, position.Currency, position.Holding, setting.Market, setting.Symbol, holding, math.Abs(holding*price)))
 			}
