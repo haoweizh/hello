8de237baf41c49386979e1425050980776578cc1
haoweizh <haoweizh@qq.com>
2025-04-01 13:45:30 +0800

    add special chan recovery

--- carry/DBHandler.go
+++ carry/DBHandler.go
@@ -128,12 +128,12 @@ func ManageConnTicks(market string) (reset bool) {
 	} else if api.RequireConnTickReset(model.AppEnvironment, market) {
 		reset = true
 		if api.GetSpecialChan(market) == 1 {
-			if market == model.Gate {
-				model.SubRecover(market, model.MarketTypePerp, model.ChanTypeMarket)
-				model.SubRecover(market, model.MarketTypeSpot, model.ChanTypeMarket)
-			} else {
-				model.SubRecover(market, ``, model.ChanTypeMarket)
-			}
+			//if market == model.Gate {
+			//	model.SubRecover(market, model.MarketTypePerp, model.ChanTypeMarket)
+			//	model.SubRecover(market, model.MarketTypeSpot, model.ChanTypeMarket)
+			//} else {
+			//	model.SubRecover(market, ``, model.ChanTypeMarket)
+			//}
 			util.Log(util.LogLevelLocal, fmt.Sprintf(`special chan reset %s`, market))
 		} else {
 			api.CreateWSTick(model.AppEnvironment, market)
