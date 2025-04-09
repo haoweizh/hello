3f1db62c26004ddf01bf5fa8b750188df5f4b696
haoweizh <haoweizh@qq.com>
2025-04-09 10:23:23 +0800

    减少clear all market infos

--- api/Gate.go
+++ api/Gate.go
@@ -411,7 +411,7 @@ var wsPriHandlerGateUnified = func(market, key string, msg []byte) {
 	if value[`R`] != nil {
 		collateral.Rate, _ = strconv.ParseFloat(value[`R`].(string), 64)
 	}
-	util.Log(util.LogLevelLocal, fmt.Sprintf("gate unified %s %f %s", collateral.AccountKey, collateral.Available, string(msg)))
+	util.LogLess(util.LogLevelLocal, fmt.Sprintf("gate unified %s %f %s", collateral.AccountKey, collateral.Available, string(msg)))
 	model.CollateralHandler(key, ``, false, collateral)
 }
 
