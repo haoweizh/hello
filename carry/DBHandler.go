61740c7a31a3d06467a749f2a573715157b1fe06
张浩伟 <zhanghaowei@edaijia-inc.cn>
2025-04-16 23:14:20 +0800

    永续资金费率刷新频率改为每小时4次

--- carry/DBHandler.go
+++ carry/DBHandler.go
@@ -205,7 +205,7 @@ func Maintain() {
 		} else {
 			c.Start()
 		}
-		_, errMarketInfo := c.AddFunc("5,35 * * * ?", cross.RefreshMarkets)
+		_, errMarketInfo := c.AddFunc("5,15,35,45 * * * ?", cross.RefreshMarkets)
 		if errMarketInfo != nil {
 			util.Log(util.LogLevelError, `fail to cron refresh market info `+errMarketInfo.Error())
 		} else {
