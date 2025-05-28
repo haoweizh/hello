64e94b60999542dacb1b6b03689ec0afe0ea6753
haoweizh <haoweizh@qq.com>
2025-05-28 20:57:12 +0800

    LAI 关仓加 score 5%

--- carry/cross/CrossCalculate.go
+++ carry/cross/CrossCalculate.go
@@ -468,6 +468,7 @@ func calcScores(statusBuy, statusSell *model.CarryStatus, marketInfoBuy, marketI
 		scoreOpen = 1
 		scoreSwitch = 1
 		scoreClose += 0.05
+		util.LogLess(util.LogLevelLocal, fmt.Sprintf(`lai score %s %s %f %f %f`, statusBuy.Market, statusBuy.Symbol, scoreOpen, scoreSwitch, scoreClose))
 	}
 	scoreMsg += fmt.Sprintf(`after handled open %f close %f buyAsk0 %f sellBid0 %f`, scoreOpen, scoreClose, tickBuy.Asks[0].Price, tickSell.Bids[0].Price)
 	return true, scoreBase, scoreOpen, scoreSwitch, scoreClose, rateBuy, rateSell, scoreMsg
